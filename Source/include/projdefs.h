8e624e74e9e3998ff1b80ff2ced1f8a4ec165d2b
Richard Barry <r.barry@freertos.org>
2006-08-27 14:09:54 +0000

    Changes ready for V4.1.0.
    git-svn-id: http://svn.code.sf.net/p/freertos/code/trunk@28 1d2547de-c912-0410-9cb9-b8ca96c0e9e2

--- Source/include/projdefs.h
+++ Source/include/projdefs.h
@@ -39,16 +39,18 @@ typedef void (*pdTASK_CODE)( void * );
 #define pdTRUE		( 1 )
 #define pdFALSE		( 0 )
 
-#define pdPASS		( 1 )
-#define pdFAIL		( 0 )
+#define pdPASS									( 1 )
+#define pdFAIL									( 0 )
+#define errQUEUE_EMPTY							( 0 )
+#define errQUEUE_FULL							( 0 )
 
 /* Error definitions. */
 #define errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY	( -1 )
 #define errNO_TASK_TO_RUN						( -2 )
-#define errQUEUE_FULL							( -3 )
 #define errQUEUE_BLOCKED						( -4 )
 #define errQUEUE_YIELD							( -5 )
 
-#endif
+#endif /* PROJDEFS_H */
+
 
 
