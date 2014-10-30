3c1bf047ae8b7ff8bdb9e70e27afa8e726fc7740
James Crosby <James.Crosby@arm.com>
2014-10-30 14:51:00 +0000

    add host system check

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -1,3 +1,10 @@
 # Copyright (C) 2014 ARM Limited. All rights reserved. 
 
 cmake_minimum_required(VERSION 2.8)
+
+# check that we are actually running on Linux, if we're not then we may pull in
+# incorrect dependencies.
+if(NOT (${CMAKE_HOST_SYSTEM_NAME} MATCHES "Linux"))
+    message(FATAL_ERROR "This Linux native target will not work on non-Linux platforms, use `yotta target` to set the target.")
+endif()
+
