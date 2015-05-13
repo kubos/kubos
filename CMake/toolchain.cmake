cbf9084bf5ebfe01ae0e6d2664cd3f1779366799
James Crosby <James.Crosby@arm.com>
2015-05-13 16:54:36 +0100

    default to std=c99

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -2,6 +2,9 @@
 
 cmake_minimum_required(VERSION 2.8)
 
+# default to C99
+set(CMAKE_C_FLAGS "-std=c99" CACHE STRING "")
+
 # check that we are actually running on Linux, if we're not then we may pull in
 # incorrect dependencies.
 if(NOT (${CMAKE_HOST_SYSTEM_NAME} MATCHES "Linux"))
