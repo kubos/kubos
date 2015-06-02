8f7e15393bf6aca86f74d0f11b22af5e2741ad48
James Crosby <James.Crosby@arm.com>
2015-06-02 16:05:35 +0100

    display which system we're running on when it isn't supported (to aid debug)

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -8,6 +8,6 @@ set(CMAKE_C_FLAGS "-std=c99" CACHE STRING "")
 # check that we are actually running on Linux, if we're not then we may pull in
 # incorrect dependencies.
 if(NOT (${CMAKE_HOST_SYSTEM_NAME} MATCHES "Linux"))
-    message(FATAL_ERROR "This Linux native target will not work on non-Linux platforms, use `yotta target` to set the target.")
+    message(FATAL_ERROR "This Linux native target will not work on non-Linux platforms (your platform is ${CMAKE_HOST_SYSTEM_NAME}), use `yotta target` to set the target.")
 endif()
 
