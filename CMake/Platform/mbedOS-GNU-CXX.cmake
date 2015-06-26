41b02ceaa9a6abf07a7ba4b9f63e377898558258
James Crosby <James.Crosby@arm.com>
2015-06-26 14:18:44 +0100

    use --specs=nano.space for newlib

--- CMake/Platform/mbedOS-GNU-CXX.cmake
+++ CMake/Platform/mbedOS-GNU-CXX.cmake
@@ -16,7 +16,7 @@ set(CMAKE_CXX_CREATE_SHARED_LIBRARY "echo 'shared libraries not supported' && 1"
 set(CMAKE_CXX_CREATE_SHARED_MODULE  "echo 'shared modules not supported' && 1")
 set(CMAKE_CXX_CREATE_STATIC_LIBRARY "<CMAKE_AR> -cr <LINK_FLAGS> <TARGET> <OBJECTS>")
 set(CMAKE_CXX_COMPILE_OBJECT        "<CMAKE_CXX_COMPILER> <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>")
-set(CMAKE_CXX_LINK_EXECUTABLE       "<CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> <LINK_LIBRARIES> -Wl,-Map,<TARGET>.map -Wl,--start-group -lnosys -lstdc++ -lsupc++ -lm -lc -lgcc -lnosys -lnosys -lstdc++ -lsupc++ -lm -lc -lgcc -lnosys -Wl,--end-group -o <TARGET>")
+set(CMAKE_CXX_LINK_EXECUTABLE       "<CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> <LINK_LIBRARIES> -Wl,-Map,<TARGET>.map -Wl,--start-group -lnosys -lstdc++ -lsupc++ -lm -lc -lgcc -lnosys -lnosys -lstdc++ -lsupc++ -lm -lc -lgcc -lnosys -Wl,--end-group  --specs=nano.specs -o <TARGET>")
 
 set(CMAKE_CXX_FLAGS_DEBUG_INIT          "-g")
 set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "-Os -DNDEBUG")
