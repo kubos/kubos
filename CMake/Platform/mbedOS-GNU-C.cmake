c0cf6257d2a4d6d7a53b54e17e603b35448e2b74
Brendan Moran <brendan.moran@arm.com>
2015-07-03 15:11:27 +0100

    Add comment to protect library grouping in cmake files

--- CMake/Platform/mbedOS-GNU-C.cmake
+++ CMake/Platform/mbedOS-GNU-C.cmake
@@ -6,6 +6,8 @@ set(CMAKE_C_CREATE_SHARED_LIBRARY "echo 'shared libraries not supported' && 1")
 set(CMAKE_C_CREATE_SHARED_MODULE  "echo 'shared modules not supported' && 1")
 set(CMAKE_C_CREATE_STATIC_LIBRARY "<CMAKE_AR> -cr <LINK_FLAGS> <TARGET> <OBJECTS>")
 set(CMAKE_C_COMPILE_OBJECT        "<CMAKE_C_COMPILER> <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>")
+# <LINK_LIBRARIES> is grouped with system libraries so that system library
+# functions (e.g. malloc) can be overridden by symbols in <LINK_LIBRARIES>
 set(CMAKE_C_LINK_EXECUTABLE       "<CMAKE_C_COMPILER> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -Wl,-Map,<TARGET>.map -Wl,--start-group <LINK_LIBRARIES> -lm -lc -lgcc -lm -lc -lgcc -Wl,--end-group  --specs=nano.specs -o <TARGET>")
 
 
@@ -21,4 +23,3 @@ set(CMAKE_ASM_FLAGS_MINSIZEREL_INIT     "-Os -DNDEBUG")
 set(CMAKE_ASM_FLAGS_RELEASE_INIT        "-Os -DNDEBUG")
 set(CMAKE_ASM_FLAGS_RELWITHDEBINFO_INIT "-Os -g -DNDEBUG")
 set(CMAKE_INCLUDE_SYSTEM_FLAG_ASM  "-isystem ")
-
--- CMake/Platform/mbedOS-GNU-CXX.cmake
+++ CMake/Platform/mbedOS-GNU-CXX.cmake
@@ -16,6 +16,8 @@ set(CMAKE_CXX_CREATE_SHARED_LIBRARY "echo 'shared libraries not supported' && 1"
 set(CMAKE_CXX_CREATE_SHARED_MODULE  "echo 'shared modules not supported' && 1")
 set(CMAKE_CXX_CREATE_STATIC_LIBRARY "<CMAKE_AR> -cr <LINK_FLAGS> <TARGET> <OBJECTS>")
 set(CMAKE_CXX_COMPILE_OBJECT        "<CMAKE_CXX_COMPILER> <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>")
+# <LINK_LIBRARIES> is grouped with system libraries so that system library
+# functions (e.g. malloc) can be overridden by symbols in <LINK_LIBRARIES>
 set(CMAKE_CXX_LINK_EXECUTABLE       "<CMAKE_CXX_COMPILER> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -Wl,-Map,<TARGET>.map -Wl,--start-group <LINK_LIBRARIES> -lstdc++ -lsupc++ -lm -lc -lgcc -lstdc++ -lsupc++ -lm -lc -lgcc -Wl,--end-group  --specs=nano.specs -o <TARGET>")
 
 set(CMAKE_CXX_FLAGS_DEBUG_INIT          "-g")
@@ -23,4 +25,3 @@ set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "-Os -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELEASE_INIT        "-Os -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-Os -g -DNDEBUG")
 set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem ")
-
