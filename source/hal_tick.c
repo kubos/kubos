a4c5334f346f7bf010c93212abf08f1bd9a78d26
Martin Kojtal <c0170@rocketmail.com>
2015-08-10 14:51:51 +0100

    Fix ticker freq. This is applied for 429zi, as it has /2 system clock.
    This might not be true for any F4 mcu, something which we should fix -
    hal tick init per platform.

--- source/hal_tick.c
+++ source/hal_tick.c
@@ -78,7 +78,11 @@ HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority) {
     // Configure time base
     TimMasterHandle.Instance = TIM_MST;
     TimMasterHandle.Init.Period            = 0xFFFFFFFF;
-    TimMasterHandle.Init.Prescaler         = (uint32_t)(SystemCoreClock / 1000000) - 1; // 1 µs tick
+    if (SystemCoreClock == 16000000) {
+        TimMasterHandle.Init.Prescaler     = (uint32_t)(SystemCoreClock / 1000000) - 1; // 1 µs tick
+    } else {
+        TimMasterHandle.Init.Prescaler     = (uint32_t)(SystemCoreClock / 2 / 1000000) - 1; // 1 µs tick
+    }
     TimMasterHandle.Init.ClockDivision     = 0;
     TimMasterHandle.Init.CounterMode       = TIM_COUNTERMODE_UP;
     TimMasterHandle.Init.RepetitionCounter = 0;
