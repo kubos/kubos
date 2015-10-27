43a66f27ce6092544cfc59679c6976ae6b067153
Alessandro Angelino <alessandro.angelino@arm.com>
2015-10-27 18:35:03 +0000

    Split HAL_Init into 2 for uVisor
    Also adding overrides

--- source/stm32f4xx_hal.c
+++ source/stm32f4xx_hal.c
@@ -166,6 +166,11 @@ static __IO uint32_t uwTick;
   */
 HAL_StatusTypeDef HAL_Init(void)
 {
+  /* Include the code that had been removed if uVisor is not present */
+#ifndef YOTTA_CFG_UVISOR_PRESENT
+  HAL_InitPre();
+#endif /* ndef YOTTA_CFG_UVISOR_PRESENT */
+
   /* Configure Flash prefetch, Instruction cache, Data cache */ 
 #if (INSTRUCTION_CACHE_ENABLE != 0)
    __HAL_FLASH_INSTRUCTION_CACHE_ENABLE();
@@ -178,9 +183,6 @@ HAL_StatusTypeDef HAL_Init(void)
 #if (PREFETCH_ENABLE != 0)
   __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
 #endif /* PREFETCH_ENABLE */
-
-  /* Set Interrupt Group Priority */
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
   /* Use systick as time base source and configure 1ms tick (default clock after Reset is HSI) */
   HAL_InitTick(TICK_INT_PRIORITY);
