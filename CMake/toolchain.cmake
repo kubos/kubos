f1f5fa8de896c0da69ada02d93de5185bca1a323
Marshall Culpepper <marshall@kubos.co>
2016-04-14 11:00:18 -0500

    restructuring for new KubOS GCC targets:
    * inherit the new arm-none-eabi-gcc target
    * rename to stm32f407-disco-gcc to be more specific about the chip in the target
    * added support for using "yotta start" to flash the discovery board

--- CMake/toolchain.cmake
+++ CMake/toolchain.cmake
@@ -13,10 +13,10 @@
 # limitations under the License.
 #
 
-if(TARGET_STM32F4_DISCO_GCC_TOOLCHAIN_INCLUDED)
+if(TARGET_STM32F407_DISCO_GCC_TOOLCHAIN_INCLUDED)
     return()
 endif()
-set(TARGET_STM32F4_DISCO_GCC_TOOLCHAIN_INCLUDED 1)
+set(TARGET_STM32F407_DISCO_GCC_TOOLCHAIN_INCLUDED 1)
 
 # provide compatibility definitions for compiling with this target: these are
 # definitions that legacy code assumes will be defined. Before adding something
@@ -24,7 +24,7 @@ set(TARGET_STM32F4_DISCO_GCC_TOOLCHAIN_INCLUDED 1)
 # definition that you're about to add to rely on the TARGET_LIKE_XXX
 # definitions that yotta provides based on the target.json file.
 #
-add_definitions("-DTARGET_STM32F4_DISCO -DTARGET_STM32F407VG -DSTM32F407xx -DTARGET_STM32F4 -DTOOLCHAIN_GCC -DTOOLCHAIN_GCC_ARM")
+add_definitions("-DTARGET_STM32F407_DISCO -DTARGET_STM32F407VG -DSTM32F407xx -DTARGET_STM32F4")
 
 # append non-generic flags, and set STM32F4-specific link script
 
