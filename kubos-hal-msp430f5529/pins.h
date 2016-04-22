60910316218db41da4af1874ad7f05063452d006
kyleparrott <kyle.parrott@gmail.com>
2016-04-22 09:18:21 -0500

    initial/stubbed out HAL implementation for msp430f5529

---
+++ kubos-hal-msp430f5529/pins.h
@@ -0,0 +1,34 @@
+/*
+ * KubOS HAL
+ * Copyright (C) 2016 Kubos Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef KUBOS_HAL_MSP430F5529_PINS_H
+#define KUBOS_HAL_MSP430F5529_PINS_H
+
+#include <msp430f5529.h>
+
+#define K_LED_0 BIT0
+#define K_LED_ORANGE BIT0
+
+#define K_LED_1 BIT6
+#define K_LED_RED BIT6 
+
+#define K_LED_COUNT 2
+
+#define K_BUTTON_0 BIT1
+
+ 
+#endif
---
+++ source/gpio.c
@@ -0,0 +1,34 @@
+/*
+ * KubOS HAL
+ * Copyright (C) 2016 Kubos Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "kubos-hal/gpio.h"
+
+#include <msp430.h>
+
+void k_gpio_init(int pin, KGPIOMode mode, KGPIOPullup pullup) {
+
+}
+
+unsigned int k_gpio_read(int pin)
+{
+    return 0;
+}
+
+void k_gpio_write(int pin, unsigned int val)
+{
+    
+}
