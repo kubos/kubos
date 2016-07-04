27761d02ec9cfdec3fbfd07c955ad0fa4375d309
Ryan Plauche <ryan@kubos.com>
2016-07-04 08:04:39 -0500

    Added file for implementing functions the cubef4 hal needs.

---
+++ source/cubef4.c
@@ -0,0 +1,30 @@
+/*
+ * KubOS HAL
+ * Copyright (C) 2016 Kubos Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @brief This file contains implementation of weak functions used by the
+ * STM32Cubef4 HAL and which are not driver specific.
+ * @author kubos.co
+ */
+
+#include <stm32cubef4/stm32f4xx_hal.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+uint32_t HAL_GetTick(void)
+{
+    return xTaskGetTickCount();
+}
