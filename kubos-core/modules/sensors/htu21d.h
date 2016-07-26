c996471f002426e2e92368ca53d97afc3d730195
Ryan Plauche <ryan@kubos.co>
2016-07-26 14:58:40 -0500

    Added example json config to header.

--- kubos-core/modules/sensors/htu21d.h
+++ kubos-core/modules/sensors/htu21d.h
@@ -21,6 +21,19 @@
  */
 /**
  * @brief HTU21D Temperature and Humidty Sensor
+ *
+ * Enabling this sensor code requires certain configuration values to be present
+ * in the application's configuration json. An example is given below:
+ *
+ *  {
+ *      "sensors": {
+ *          "htu21d": {
+ *              "i2c_bus": "K_I2C1"
+ *          }
+ *      }
+ *  }
+ *
+ * This would enable the htu21d sensor code and configure it for the I2C bus K_I2C1.
  */
 #ifdef YOTTA_CFG_SENSORS_HTU21D
 #ifndef HTU21D_H
