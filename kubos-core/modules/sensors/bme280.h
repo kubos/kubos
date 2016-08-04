2dadd09856c2750bf88f1d634bf6262c4a6b1058
itchy5 <mitch.grabner@gmail.com>
2016-08-04 10:47:26 -0500

    removed kubos license header

--- kubos-core/modules/sensors/bme280.h
+++ kubos-core/modules/sensors/bme280.h
@@ -13,23 +13,9 @@
 
   Written by Limor Fried & Kevin Townsend for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
+
+  Modified by KubOS Corporation 2016 for integration into Kubos Core
  ***************************************************************************/
- /*
-  * KubOS Core
-  * Copyright (C) 2016 Kubos Corporation
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
  /**
   * @defgroup SENSORS
   * @addtogroup SENSORS
