f914f321ccd740e2b0e3a34865dc50f8612d183f
kyleparrott <kyle@kubos.co>
2016-10-25 14:58:31 -0500

    KUBOS-187 Adding Debug Configuration for the stm32f407 discovery target

--- targets/target-stm32f407-disco-gcc/openocd/flash.sh
+++ targets/target-stm32f407-disco-gcc/openocd/flash.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
 this_dir=$(cd "`dirname "$0"`"; pwd)
-cmd=$1
+program=$1 # program is fist becasue for debugging there is not a command provided
+           # openocd will start a gdb server by default if there's not a command provided
+cmd=$2
+if [[ ! -z $cmd ]]; then
+    openocd_arg="$cmd $program"
+fi
 unamestr=`uname`
 
 if [[ "$unamestr" =~ "Linux" ]]; then
-	device=`lsusb -d '0483:'`
+    device=`lsusb -d '0483:'`
 elif [[ "$unamestr" =~ "Darwin" ]]; then
-	device=`python $this_dir/osxusb.py -d '0483:'`
+    device=`python $this_dir/osxusb.py -d '0483:'`
 fi
 
 if [[ "$device" =~ "3748" ]]; then
-	cfg="stm32f407vg.cfg"
+    cfg="stm32f407vg.cfg"
 fi
 
 if [[ "$device" =~ "374b" ]]; then
-	cfg="stm32f407g-disc1.cfg"
+    cfg="stm32f407g-disc1.cfg"
 fi
-
 
 if [[ ! -z $cfg ]]; then
-	echo openocd -f $this_dir/$cfg -c \"$cmd\"
-	openocd -f $this_dir/$cfg -c "$cmd"
+    if [[ ! -z $openocd_arg ]]; then #Flashing the target
+        echo openocd -f $this_dir/$cfg -c \"$openocd_arg\"
+        openocd -f $this_dir/$cfg -c "$openocd_arg"
+    else                            #Debugging the Target
+        (openocd -f $this_dir/$cfg -c "$openocd_arg" &) #Start the gdb server in a sub-shell
+        sleep 3
+        arm-none-eabi-gdb -ex "target remote localhost:3333" -ex "file $program"
+    fi
 else
-	echo "No compatible ST-Link device found"
+    echo "No compatible ST-Link device found"
 fi
+
+
+
