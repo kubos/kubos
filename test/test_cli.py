8f2170d0f91d7b2c4d85f526d2163ca5076a6cc2
Dave Sims <davesims@github.com>
2016-10-27 13:02:24 -0500

    Moved test directory to root

---
+++ test/test_cli.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestCLI(unittest.TestCase):
+
+    def test_kubos_installed(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
