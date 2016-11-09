f3e6b6653b15c426e9f0db31c6adf0889560e2dd
cnalley <cnalley@kubos.co>
2016-11-09 15:45:24 -0800

    Added a union data type to the telemetry packet struct.

--- telemetry/telemetry/telemetry.h
+++ telemetry/telemetry/telemetry.h
@@ -4,13 +4,31 @@
 #include <stdint.h>
 
 /**
- * Telemetry packet routing information structure
+ * Telemetry packet routing information structure.
  */
 typedef struct
 {
     uint8_t source_id;
     uint8_t dest_flag;
+    uint8_t data_type;
 } telemetry_source;
+
+/**
+ * Telemetry data types.
+ */
+typedef enum {
+    TELEMETRY_TYPE_INT = 0,
+    TELEMETRY_TYPE_FLOAT
+} telemetry_type;
+
+/**
+ * Telemetry union for data.
+ */
+typedef union
+{
+    int i;
+    float f;
+} telem_union;
 
 /**
  * Basic telemetry packet structure - encapsulating routing information
@@ -19,8 +37,8 @@ typedef struct
 typedef struct
 {
     telemetry_source source;
+    telem_union data;
     uint16_t timestamp;
-    uint16_t data;
 } telemetry_packet;
 
 /**
