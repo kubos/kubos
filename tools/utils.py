9b3dd4f15f6b91e611e43f634d232824285e9f05
Ryan Plauche <ryan@kubos.co>
2017-02-07 14:14:57 -0600

    Updated list-changed-modules command to take parameter. Added buildTarget value to targets which should be used for building. Clean up from code review.

---
+++ tools/utils.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+import sys
+
+def cmd(*args, **kwargs):
+    cwd = kwargs.get('cwd', os.getcwd())
+    save_output = kwargs.pop('save_output', False)
+    echo = kwargs.pop('echo', True)
+
+    if echo:
+        print ' '.join(args)
+    try:
+        if save_output:
+            return subprocess.check_output(args, **kwargs)
+        else:
+            return subprocess.check_call(args, **kwargs)
+    except subprocess.CalledProcessError, e:
+        if echo:
+            print >>sys.stderr, 'Error executing command, giving up'
+        return 1
+
+
+# def cmd(*args, **kwargs):
+#         cwd = kwargs.get('cwd', os.getcwd())
+#         save_output = kwargs.pop('save_output', False)
+#         echo = kwargs.pop('echo', True)
+
+#         if echo:
+#             print ' '.join(args)
+#         try:
+#             if save_output:
+#                 return subprocess.check_output(args, **kwargs)
+#             else:
+#                 return subprocess.check_call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
+#         except subprocess.CalledProcessError, e:
+#             #print('Error executing command, giving up',file=sys.stderr)
+#             return 1
