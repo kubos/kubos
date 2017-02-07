f41bc03c771006a826a2599d375a80dc2a77256a
Ryan Plauche <ryan@kubos.co>
2017-02-07 14:14:57 -0600

    Updated list-changed-modules command to take parameter. Added buildTarget value to targets which should be used for building. Clean up from code review.

--- tools/build.py
+++ tools/build.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 import sys
 from kubos_build import KubosBuild
+import utils
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
 root_dir = os.path.dirname(this_dir)
@@ -16,32 +17,14 @@ class KubosBuilder(object):
         self.modules = self.kb.modules()
         self.targets = self.kb.targets()
 
-    def cmd(self, *args, **kwargs):
-        cwd = kwargs.get('cwd', os.getcwd())
-        save_output = kwargs.pop('save_output', False)
-        echo = kwargs.pop('echo', False)
-
-        if echo:
-            print(' '.join(args))
-        try:
-            if save_output:
-                return subprocess.check_output(args, **kwargs)
-            else:
-                return subprocess.check_call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
-        except subprocess.CalledProcessError, e:
-            #print('Error executing command, giving up',file=sys.stderr)
-            return 1
-
     def build(self, module_name="", target_name=""):
-        #module_list = next((m for m in self.kb.modules() if m.yotta_name() == module_name), None)
         module = next((m for m in self.kb.modules() if m.yotta_name() == module_name), None)
         target = next((t for t in self.kb.targets() if t.yotta_name() == target_name), None)
         if module and target:
             print('Building [module %s@%s] for [target %s] - ' % (module.yotta_name(), module.path, target_name), end="")
-            self.cmd('kubos', 'target', target_name, cwd=module.path)
-            self.cmd('kubos', 'clean', cwd=module.path)
-            ret = self.cmd('yt', 'build', cwd=module.path)
-            #print('Passed' if ret is 0 else 'Failed')
+            utils.cmd('kubos', 'target', target_name, cwd=module.path, echo=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            utils.cmd('kubos', 'clean', cwd=module.path, echo=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            ret = utils.cmd('yt', 'build', cwd=module.path, echo=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             print('Result %d' % ret)
             return ret
         else:
@@ -53,7 +36,8 @@ class KubosBuilder(object):
 
     def list_targets(self):
         for target in self.kb.targets():
-            print(target.yotta_name())
+            if 'buildTarget' in target.yotta_data:
+                print(target.yotta_name())
 
     def list_modules(self):
         for module in self.kb.modules():
@@ -65,7 +49,7 @@ class KubosBuilder(object):
         # Pop off file name for first directory
         path_list.pop()
         while len(path_list):
-            new_path = "" + "/".join(path_list)
+            new_path = "/".join(path_list)
             kubos_build = KubosBuild(kubos_dir=new_path)
             for p in kubos_build.projects:
                 if p.type != "unknown":
@@ -76,9 +60,9 @@ class KubosBuilder(object):
             path_list.pop()
         return modules
 
-    def list_changed_modules(self):
+    def list_changed_modules(self, ref):
         try:
-            git_output = subprocess.check_output(["git", "diff", "--numstat", "HEAD^!"])
+            git_output = subprocess.check_output(["git", "diff", "--numstat", ref])
             git_lines = [l for l in git_output.splitlines()]
             file_paths = [l.split()[2] for l in git_lines]
             modules = set()
@@ -86,7 +70,7 @@ class KubosBuilder(object):
                 modules = modules | (self.find_modules(path))
             
             if len(modules):
-                print("Modules changed in last commit:")
+                print("Modules changed:")
             for m in modules:
                 print(m)
         except subprocess.CalledProcessError:
@@ -95,14 +79,20 @@ class KubosBuilder(object):
     def build_all_targets(self, module_name=""):
         module = next((m for m in self.kb.modules() if m.yotta_name() == module_name), None)
         if module:
-            for target in self.kb.targets():
+            for target in self.kb.build_targets():
                 self.build(module.yotta_name(), target.yotta_name())
+        else:
+            print("Module %s was not found" % module_name)
+            return 1
 
     def build_all_modules(self, target_name=""):
         target = next((t for t in self.kb.targets() if t.yotta_name() == target_name), None)
         if target:
             for module in self.kb.modules():
                 self.build(module.yotta_name(), target.yotta_name())
+        else:
+            print("Target %s was not found" % target_name)
+            return 1
     
     def build_all_combinations(self):
         for target in self.kb.targets():
@@ -111,19 +101,24 @@ class KubosBuilder(object):
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Builds KubOS modules')
+        description='Builds Kubos modules')
 
     parser.add_argument('--target', metavar='target',
                         help='Specifies target to build modules for')
     parser.add_argument('--module', metavar='module',
                         help='Specifies modules to build')
-    parser.add_argument('--all_targets', action='store_true', default=False,
+    parser.add_argument('--all-targets', action='store_true', default=False,
                         help='Builds module for all targets')
-    parser.add_argument('--all_modules', action='store_true', default=False,
+    parser.add_argument('--all-modules', action='store_true', default=False,
                         help='Builds all modules for target')
-    parser.add_argument('--list_targets', action='store_true', default=False)
-    parser.add_argument('--list_modules', action='store_true', default=False)
-    parser.add_argument('--list_changed_modules', action='store_true', default=False)
+    parser.add_argument('--list-targets', action='store_true', default=False,
+                        help='Lists all targets available for building')
+    parser.add_argument('--list-modules', action='store_true', default=False,
+                        help='Lists all modules found')
+    parser.add_argument('--list-changed-modules', action="store", nargs="?", 
+                        dest="list_changed_modules", const="HEAD^!",
+                        help='Lists modules that have changed. By default will diff against '
+                        'the last commit. The git diff path desired can also be passed in')
         
 
     args = parser.parse_args()
@@ -133,20 +128,21 @@ def main():
     ret = 0
 
     if args.list_targets:
-        builder.list_targets()
-    if args.list_modules:
-        builder.list_modules()
-    if args.list_changed_modules:
-        builder.list_changed_modules()
-    if args.target and args.module:
+        ret = builder.list_targets()
+    elif args.list_modules:
+        ret = builder.list_modules()
+    elif args.list_changed_modules:
+        ret = builder.list_changed_modules(args.list_changed_modules)
+    elif args.target and args.module:
         ret = builder.build(module_name=args.module, target_name=args.target)
     elif args.module and args.all_targets:
-        builder.build_all_targets(module_name=args.module)
+        ret = builder.build_all_targets(module_name=args.module)
     elif args.target and args.all_modules:
-        builder.build_all_modules(target_name=args.target)
+        ret = builder.build_all_modules(target_name=args.target)
     elif args.all_targets and args.all_modules:
-        builder.build_all_combinations()
+        ret = builder.build_all_combinations()
     else:
+        parser.print_help()
         ret = -1
 
     sys.exit(ret)
--- tools/kubos_build.py
+++ tools/kubos_build.py
@@ -4,6 +4,8 @@ import json
 import os
 import subprocess
 import sys
+import glob
+import utils
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
 kubos_dir = os.path.dirname(this_dir)
@@ -22,9 +24,6 @@ class Project(object):
             self.yotta_data = json.load(open(path + '/target.json', 'r'))
             self.type = 'yotta_target'
 
-        #self.commit = self.get_commit_sha()
-        #self.upstream = self.find_upstream_branch()
-        #self.tag = self.get_last_tag()
         self.version = self.get_version()
 
     def is_bin(self):
@@ -94,24 +93,30 @@ class KubosBuild(object):
 
     def targets(self):
         return filter(lambda c: c.type == 'yotta_target', self.projects)
+    
+    def build_targets(self):
+        return filter(lambda c: 'buildTarget' in c.yotta_data, self.targets())
 
     def find_projects(self):
         self.projects = []
-        modules = subprocess.check_output(["find", ".", "-name",
-                                        "module.json"], cwd=self.kubos_dir)
-        for path in modules.splitlines():
-            if "yotta_modules" not in path:
-                path = path.replace("module.json", "").strip()
-                name = path.split("/")[-2]
-                path = os.path.abspath(self.kubos_dir + "/" + path)
-                self.projects.append(Project(name, path))
+        try:
+            modules = subprocess.check_output(["find", ".", "-name",
+                                            "module.json"], cwd=self.kubos_dir)
+            for path in modules.splitlines():
+                if "yotta_modules" not in path:
+                    path = path.replace("module.json", "").strip()
+                    name = path.split("/")[-2]
+                    path = os.path.abspath(self.kubos_dir + "/" + path)
+                    self.projects.append(Project(name, path))
 
-        modules = subprocess.check_output(["find", ".", "-name",
-                                        "target.json"], cwd=self.kubos_dir)
-        for path in modules.splitlines():
-            if "yotta_targets" not in path:
-                path = path.replace("target.json", "").strip()
-                name = path.split("/")[-2]
-                path = os.path.abspath(self.kubos_dir + "/" + path)
-                self.projects.append(Project(name, path))
+            modules = subprocess.check_output(["find", ".", "-name",
+                                            "target.json"], cwd=self.kubos_dir)
+            for path in modules.splitlines():
+                if "yotta_targets" not in path:
+                    path = path.replace("target.json", "").strip()
+                    name = path.split("/")[-2]
+                    path = os.path.abspath(self.kubos_dir + "/" + path)
+                    self.projects.append(Project(name, path))
+        except OSError:
+            pass
 
