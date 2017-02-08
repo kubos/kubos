b7b36b4bb922f8fb8ea106132f0c67d74cba10c0
Ryan Plauche <ryan@kubos.co>
2017-02-08 14:58:52 -0600

    Removing commented out code.

--- tools/kubos_link.py
+++ tools/kubos_link.py
@@ -15,22 +15,6 @@ class KubosLinker(object):
     def __init__(self):
         self.kb = KubosBuild()
 
-    # def cmd(self, *args, **kwargs):
-    #     cwd = kwargs.get('cwd', os.getcwd())
-    #     save_output = kwargs.pop('save_output', False)
-    #     echo = kwargs.pop('echo', True)
-
-    #     if echo:
-    #         print ' '.join(args)
-    #     try:
-    #         if save_output:
-    #             return subprocess.check_output(args, **kwargs)
-    #         else:
-    #             return subprocess.check_call(args, **kwargs)
-    #     except subprocess.CalledProcessError, e:
-    #         print >>sys.stderr, 'Error executing command, giving up'
-    #         sys.exit(1)
-
     def link_sys(self, link_cmd):
         for module in self.kb.modules(include_bin=False):
             print '[module %s@%s]' % (module.yotta_name(), module.path)
