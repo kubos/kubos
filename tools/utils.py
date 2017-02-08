3aa15a026cad96d684b95f3240a09dab6adb0f61
Ryan Plauche <ryan@kubos.co>
2017-02-08 14:58:52 -0600

    Removing commented out code.

--- tools/utils.py
+++ tools/utils.py
@@ -18,20 +18,3 @@ def cmd(*args, **kwargs):
         if echo:
             print >>sys.stderr, 'Error executing command, giving up'
         return 1
-
-
-# def cmd(*args, **kwargs):
-#         cwd = kwargs.get('cwd', os.getcwd())
-#         save_output = kwargs.pop('save_output', False)
-#         echo = kwargs.pop('echo', True)
-
-#         if echo:
-#             print ' '.join(args)
-#         try:
-#             if save_output:
-#                 return subprocess.check_output(args, **kwargs)
-#             else:
-#                 return subprocess.check_call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
-#         except subprocess.CalledProcessError, e:
-#             #print('Error executing command, giving up',file=sys.stderr)
-#             return 1
