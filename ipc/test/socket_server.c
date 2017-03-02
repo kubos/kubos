22f3476db01be6d893225f1cbde690056c2521e2
Ryan Plauche <ryan@kubos.co>
2017-03-02 13:34:58 -0600

    Layering back in unit test.

---
+++ ipc/test/socket_server.c
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2017 Kubos Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ipc/pubsub.h>
+#include <csp/csp.h>
+#include <csp/drivers/socket.h>
+#include <csp/interfaces/csp_if_socket.h>
+#include <tinycbor/cbor.h>
+#include <cmocka.h>
+
+#define TEST_INT_PORT 10
+#define TEST_EXT_PORT 20
+#define TEST_NUM_CON 5
+#define TEST_ADDRESS 1
+#define TEST_SOCKET_PORT 8888
+
+static char send_msg[] = "test123test";
+static char recv_msg[12];
+
+CSP_DEFINE_TASK(client_task)
+{
+    pubsub_conn conn;
+
+    while (!kprv_subscriber_socket_connect(&conn, TEST_ADDRESS, TEST_EXT_PORT)) {
+        csp_sleep_ms(10);
+    }
+
+    kprv_send_csp(&conn, send_msg, 12);
+
+    kprv_subscriber_socket_close(&conn);
+
+    return CSP_TASK_RETURN;
+}
+
+
+static void test_socket_server(void ** arg)
+{
+    csp_socket_t * int_socket = NULL;
+    csp_socket_t * ext_socket = NULL;
+    csp_thread_handle_t client_task_handle;
+    pubsub_conn conn = {
+        .conn_handle = NULL
+    };
+    char buffer[10];
+
+    csp_buffer_init(20, 256);
+
+    /* Init CSP with address MY_ADDRESS */
+    csp_init(TEST_ADDRESS);
+
+    /* Start router task with 500 word stack, OS task priority 1 */
+    csp_route_start_task(0, 0);
+
+    csp_thread_create(client_task, "CLIENT", 1024, NULL, 0, &client_task_handle);   
+
+    ext_socket = kprv_server_setup(TEST_EXT_PORT, 20);
+    assert_non_null(ext_socket);
+
+    assert_true(kprv_server_socket_accept(ext_socket, &conn));
+    assert_non_null(conn.conn_handle);
+
+    assert_true(kprv_publisher_read(&conn, recv_msg, 12, TEST_EXT_PORT));
+    assert_string_equal(send_msg, recv_msg);
+
+    csp_close_socket(ext_socket);
+
+    kprv_subscriber_socket_close(&conn);
+
+    csp_thread_kill(client_task_handle);
+
+    csp_route_end_task();
+
+    csp_terminate();
+
+    csp_buffer_cleanup();
+}
+
+
+int main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_socket_server),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
