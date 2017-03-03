79603b550abd7fe4259f4b6c9c116f8c1238c05d
kyleparrott <kyle@kubos.co>
2017-03-03 11:31:20 -0600

    adding file header copyrights

--- cmd-control-daemon/cmd-control-daemon/daemon.h
+++ cmd-control-daemon/cmd-control-daemon/daemon.h
@@ -1,8 +1,30 @@
+/*
+* Copyright (C) 2017 Kubos Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #ifndef DAEMON_H
 #define DAEMON_H
 #include <csp/csp.h>
 #include <command-and-control/types.h>
 #include <tinycbor/cbor.h>
+
+#ifdef YOTTA_CFG_CNC_DAEMON_CMD_STR_LEN
+#define CMD_STR_LEN YOTTA_CFG_CNC_DAEMON_CMD_STR_LEN
+#else
+#define CMD_STR_LEN 75
+#endif
 
 typedef int (*lib_function)(int, char**);
 
--- cmd-control-daemon/source/main.c
+++ cmd-control-daemon/source/main.c
@@ -1,3 +1,19 @@
+/*
+* Copyright (C) 2017 Kubos Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #include <argp.h>
 #include <csp/csp.h>
 #include <csp/csp_interface.h>
@@ -151,7 +167,7 @@ int main(int argc, char **argv)
 {
     int my_address = 1;
     csp_socket_t *sock;
-    char command_str[75];
+    char command_str[CMD_STR_LEN];
     CNCCommandPacket command;
     CNCResponsePacket response;
     //The wrapper keeps track of a command input, its result and
