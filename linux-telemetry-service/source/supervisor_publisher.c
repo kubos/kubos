f8330146a7f26285d28d1bd9617946bf90df316f
Ryan Plauche <ryan@kubos.co>
2017-04-05 11:38:50 -0500

    Added publisher for supervisor metrics.

---
+++ linux-telemetry-service/source/supervisor_publisher.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 Kubos Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <isis-iobc-hal/supervisor.h>
+#include <telemetry/telemetry.h>
+#include <csp/arch/csp_time.h>
+
+#include "publishers.h"
+
+CSP_DEFINE_TASK(supervisor_publisher)
+{
+    while (1)
+    {
+        supervisor_housekeeping_t info;
+        if (supervisor_get_housekeeping(&info))
+        {
+            telemetry_publish((telemetry_packet) {
+                .data = info.fields.iobcUptime,
+                .timestamp = csp_get_ms(),
+                .source = YOTTA_CFG_TELEMETRY_PUBLISHERS_SUPERVISOR_UPTIME
+            });
+
+            telemetry_publish((telemetry_packet) {
+                .data = info.fields.iobcResetCount,
+                .timestamp = csp_get_ms(),
+                .source = YOTTA_CFG_TELEMETRY_PUBLISHERS_SUPERVISOR_RESET_COUNT
+            });
+        }
+
+        sleep(10);
+    }
+}
