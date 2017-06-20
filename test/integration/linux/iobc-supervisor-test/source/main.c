18c0a07797f91f36c2984de48735b753f9995884
Ryan Plauche <ryan@kubos.co>
2017-06-20 11:16:49 -0500

    Modified supervisor test to verify version.

--- test/integration/linux/iobc-supervisor-test/source/main.c
+++ test/integration/linux/iobc-supervisor-test/source/main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-#include <fcntl.h>
 #include <stdlib.h>
-
 #include "kubos-hal-iobc/supervisor.h"
 
-int main(int argc, char **argv) 
+int main(int argc, char ** argv)
 {
-    supervisor_version_t version = {0};
+    supervisor_version_t version = { 0 };
     if (supervisor_get_version(&version))
     {
-        printf("Supervisor Communication Successful!\n");
-    }
-    else
-    {
-        printf("Supervisor Communication Failed!\n");
+        if ((version.fields.major_version == 53)
+            && (version.fields.minor_version == 53)
+            && (version.fields.patch_version == 48))
+        {
+            printf("Supervisor Communication Successful!\n");
+            return 0;
+        }
     }
 
-	return 0;
+    printf("Supervisor Communication Failed!\n");
+
+    return 1;
 }
