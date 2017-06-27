6cdde9b3835ce84bb8de56e0d69cbad2aa2acd2f
Ryan Plauche <ryan@kubos.co>
2017-06-27 13:55:20 -0500

    Code review fix ups.

--- services/telemetry/telemetry-linux/telemetry-linux/msg.h
+++ services/telemetry/telemetry-linux/telemetry-linux/msg.h
@@ -24,7 +24,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <telemetry/types.h>
-#include <tinycbor/cbor.h>
+#include <tinycbor/CBOR.h>
 
 /**
  * Parses out the message type from an encoded message
@@ -55,7 +55,7 @@ bool telemetry_parse_packet_msg(const uint8_t * buffer, uint32_t buffer_size, te
 /**
  * Attempts to encode a subscribe message
  * @param[out] buffer buffer to store encoded packet in
- * @param[in] topic_id topic id for subscribing
+ * @param[in] topic_id topic ID for subscribing
  * @return int 0 if successful, otherwise negative error code
  */
 int telemetry_encode_subscribe_msg(uint8_t * buffer, const uint16_t * topic_id);
@@ -64,7 +64,7 @@ int telemetry_encode_subscribe_msg(uint8_t * buffer, const uint16_t * topic_id);
  * Attempt to parse a subscribe message
  * @param[in] buffer buffer storing packet data
  * @param[in] buffer_size size of buffer
- * @param[out] topic_id topic id read from message
+ * @param[out] topic_id topic ID read from message
  * @return bool true if successful, otherwise false
  */
 bool telemetry_parse_subscribe_msg(const uint8_t * buffer, uint32_t buffer_size, uint16_t * topic_id);
@@ -72,7 +72,7 @@ bool telemetry_parse_subscribe_msg(const uint8_t * buffer, uint32_t buffer_size,
 /**
  * Attempts to encode an unsubscribe message
  * @param[out] buffer buffer to store encoded packet in
- * @param[in] topic_id topic id to encode in message
+ * @param[in] topic_id topic ID to encode in message
  * @return int 0 if successful, otherwise negative error code
  */
 int telemetry_encode_unsubscribe_msg(uint8_t * buffer, const uint16_t * topic_id);
@@ -81,7 +81,7 @@ int telemetry_encode_unsubscribe_msg(uint8_t * buffer, const uint16_t * topic_id
  * Attempt to parse an unsubscribe message
  * @param[in] buffer buffer storing packet data
  * @param[in] buffer_size size of buffer
- * @param[out] topic_id topic id read from message
+ * @param[out] topic_id topic ID read from message
  * @return bool true if successful, otherwise false
  */
 bool telemetry_parse_unsubscribe_msg(const uint8_t * buffer, uint32_t buffer_size, uint16_t * topic_id);
@@ -95,8 +95,8 @@ int telemetry_encode_disconnect_msg(uint8_t * buffer);
 
 /**
  * Sets up the structures for encoding a message
- * @param[out] encoder Master cbor encoder
- * @param[out] container cbor container for map
+ * @param[out] encoder Master CBOR encoder
+ * @param[out] container CBOR container for map
  * @param[out] buffer buffer to store end data in
  * @param[in] buffer_size size of buffer
  * @param[in] num_elements number of elements in the message map
@@ -106,10 +106,10 @@ int telemetry_encode_disconnect_msg(uint8_t * buffer);
 int start_encode_msg(CborEncoder * encoder, CborEncoder * container, uint8_t * buffer, uint32_t buffer_size, uint8_t num_elements, telemetry_message_type message_type);
 
 /**
- * Finishing the cbor encoding process
+ * Finishing the CBOR encoding process
  * @param[in,out] buffer buffer for storing serialized data
- * @param[in,out] encoder cbor encoder
- * @param[in,out] container cbor map container
+ * @param[in,out] encoder CBOR encoder
+ * @param[in,out] container CBOR map container
  * @return int 0 if successful, otherwise negative error code
  */
 int end_encode_msg(uint8_t * buffer, CborEncoder * encoder, CborEncoder * container);
