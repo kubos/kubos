994a293e1fd4b2edb43f68529c2d1b2570b234ed
Catherine Freed <catherine@kubos.co>
2017-08-23 10:38:09 -0500

    Removing unneeded includes

--- examples/kubos-linux-uarttx/source/main.c
+++ examples/kubos-linux-uarttx/source/main.c
@@ -24,17 +24,11 @@
  *
  */
 
-#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <math.h>
-#include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
