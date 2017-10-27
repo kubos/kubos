e3e0bf26b585e22f0e72a9982f0d049e3659da47
Ryan Plauche <ryan@kubos.co>
2017-10-27 11:40:48 -0500

    Modifications to docker container to start d-bus daemon on init when running circle tests.

--- evented-control/scripts/pretest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start D-Bus system daemon
-dbus-daemon --config-file=./scripts/sess.conf --fork --nopidfile --address=unix:path=/tmp/kubos
-
-# Set D-Bus related environment variables
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/tmp/kubos
-export DBUS_STARTER_BUS_TYPE=session
