e36a457b09640b871da019e792b7a3cdb945f244
Ryan Plauche <ryan@kubos.co>
2017-10-27 14:22:18 -0500

    Modified dbus script and Dockerfile to set env vars.

--- tools/dist/dbus_environment.sh
+++ tools/dist/dbus_environment.sh
@@ -2,7 +2,3 @@
 
 # Start D-Bus system daemon
 dbus-daemon --config-file=/etc/dbus-1/kubos.conf --fork --nopidfile --address=unix:path=/tmp/kubos
-
-# Export D-bus variables
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/tmp/kubos
-export DBUS_STARTER_BUS_TYPE=session
