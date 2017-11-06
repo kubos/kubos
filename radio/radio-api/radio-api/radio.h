67619685fdce8c45d0883c3d29a2ac5604a58fe5
Catherine Garabedian <catherine@kubos.co>
2017-11-06 10:33:20 -0600

    Creating generic radio-impl include

--- radio/radio-api/radio-api/radio.h
+++ radio/radio-api/radio-api/radio.h
@@ -23,8 +23,10 @@
 
 #include <stdint.h>
 #include "radio-struct.h"
+#include "radio-impl.h"
 
 /* Define the radio-specific structures/enumerators */
+/*
 #if defined(YOTTA_CFG_RADIO_TRXVU) && !defined(HAVE_RADIO)
 #define HAVE_RADIO
 #include <trxvu-radio-api/radio.h>
@@ -32,6 +34,7 @@
 #ifndef HAVE_RADIO
 #error No radio defined!
 #endif
+*/
 
 /* Define the global functions */
 /**
