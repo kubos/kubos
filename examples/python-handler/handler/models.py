0e0b066d60666b868afa08f68019a85878edcb71
Ryan Plauche <ryan@kubos.co>
2017-11-15 14:56:49 -0600

    Example python-based subsystem handler.

---
+++ examples/python-handler/handler/models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Kubos Corporation
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+"""
+Graphene ObjectType classes for subsystem modeling.
+"""
+
+import graphene
+
+class Subsystem(graphene.ObjectType):
+    """
+    Model encapsulating subsystem functionality.
+    """
+
+    power_on = graphene.Boolean()
+
+    def refresh(self):
+        """
+        Will hold code for refreshing the status of the subsystem
+        model based on queries to the actual hardware.
+        """
+
+        print "Querying for subsystem status"
+        self.power_on = not self.power_on
