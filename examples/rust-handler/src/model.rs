3e88f17ff324b37495e102c4a538c69e18b8a000
Ryan Plauche <ryan@kubos.co>
2017-12-01 09:47:20 -0600

    Cleaned up code. Removed faked out data via persistent. Added stub helper functions for fetching device data.

--- examples/rust-handler/src/model.rs
+++ examples/rust-handler/src/model.rs
@@ -1,13 +1,50 @@
+//
+// Copyright (C) 2017 Kubos Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/// Model for handler's subsystem
 pub struct Subsystem {
-    pub power: bool
+    power: bool,
+    uptime: i32,
 }
 
 impl Subsystem {
+    /// Creates new Subsystem structure instance
+    /// Code querying for new subsystem data could
+    /// be used here to populate structure
+    pub fn new() -> Subsystem {
+        println!("getting new subsystem data");
+        Subsystem {
+            power: true,
+            uptime: 100,
+        }
+    }
+
+    /// Power status getter
+    /// Code querying for new power value
+    /// could be placed here
     pub fn power(&self) -> bool {
+        println!("getting power");
         self.power
     }
 
-    pub fn me(&self) -> &Subsystem {
-        self
+    /// Uptime getter
+    /// Code querying for new uptime value
+    /// could be placed here
+    pub fn uptime(&self) -> i32 {
+        println!("getting uptime");
+        self.uptime
     }
 }
--- examples/rust-handler/src/schema.rs
+++ examples/rust-handler/src/schema.rs
@@ -1,26 +1,57 @@
-use model::{Subsystem};
-use juniper::{Context as JuniperContext};
+//
+// Copyright (C) 2017 Kubos Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
+use model::Subsystem;
+use juniper::Context as JuniperContext;
+
+/// Context used to pass global data into Juniper queries
 pub struct Context {
-    pub subsystem : Subsystem
+    pub subsystem: Subsystem,
 }
 
-impl JuniperContext for Context { }
+impl JuniperContext for Context {}
 
+impl Context {
+    /// Give us a reference to subsystem for passing
+    /// along the Juniper chain
+    pub fn get_subsystem(&self) -> &Subsystem {
+        &self.subsystem
+    }
+}
+
+/// GraphQL model for Subsystem
 graphql_object!(Subsystem: Context as "Subsystem" |&self| {
     description: "Handler subsystem"
 
     field power() -> bool as "Power state of subsystem" {
         self.power()
     }
+
+    field uptime() -> i32 as "Uptime of subsystem" {
+        self.uptime()
+    }
 });
 
 pub struct QueryRoot;
 
+/// GraphQL model for base query
 graphql_object!(QueryRoot : Context as "Query" |&self| {
     field subsystem(&executor) -> Option<&Subsystem>
-        as "Get power status"
+        as "Subsystem query"
     {
-        Some(executor.context().subsystem.me())
+        Some(executor.context().get_subsystem())
     }
 });
