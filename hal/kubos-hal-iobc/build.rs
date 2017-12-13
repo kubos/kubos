ff2cefcae83928723e69a220a572e98a60c2b5ce
Ryan Plauche <ryan@kubos.co>
2017-12-13 13:54:11 -0600

    Modified kubos-hal-iobc build script to place library in OUT_DIR.

--- hal/kubos-hal-iobc/build.rs
+++ hal/kubos-hal-iobc/build.rs
@@ -1,11 +1,13 @@
 /// A custom build.rs for building out the kubos-hal-iobc
 /// yotta module and exporting it as a Rust crate
 
+use std::env;
 use std::process::Command;
 
 /// The majority of the code here is hard-coded boilerplate
 /// and should be moved out into another kubos-build crate
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
     // Can we attach this to cargo clean?
     Command::new("kubos").args(&["clean"]).status().unwrap();
 
@@ -16,20 +18,21 @@ fn main() {
         .status()
         .unwrap();
 
+    let out_path = format!("{}/libkubos-hal-iobc.a", out_dir);
+
     // The default behavior of rustc-link-lib=static
     // is to look for a library named lib{name}.a
+    // We also need to move the library into the OUT_DIR
+    // so that the module building can find it
     Command::new("cp")
         .args(
-            &[
-                "build/x86-linux-native/source/kubos-hal-iobc.a",
-                "build/x86-linux-native/source/libkubos-hal-iobc.a",
-            ],
+            &["build/x86-linux-native/source/kubos-hal-iobc.a", &out_path],
         )
         .status()
         .unwrap();
 
     // We need to figure out how to infer this information from
     // the information in the ./build folder
-    println!("cargo:rustc-link-search=native=build/x86-linux-native/source");
+    println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=kubos-hal-iobc");
 }
