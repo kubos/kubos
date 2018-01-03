90dd985736cfcef2158d3bfb2ce8624e3cf380d9
Ryan Plauche <ryan@kubos.co>
2018-01-03 15:42:06 -0600

    Parameter and message cleanup per code review.

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,12 @@ fn target_converter(kubos_target: &str) -> String {
         "kubos-linux-isis-gcc" => String::from("arm-unknown-linux-gnueabi"),
         _ => {
             panic!(
-                "Target not supported for cargo/yotta builds {}",
+                "Target '{}' not supported for cargo/yotta builds\
+                 \nCurrently supported targets are:\
+                 \nx86-linux-native\
+                 \nkubos-linux-beaglebone-gcc\
+                 \nkubos-linux-pumpkin-mbm2-gcc\
+                 \nkubos-linux-isis-gcc",
                 kubos_target
             )
         }
@@ -39,9 +44,14 @@ fn target_converter(kubos_target: &str) -> String {
 
 /// Perform `cargo build` using the proper
 /// Rust/Clang target triplet
-fn cargo_build(target: &str) {
+fn cargo_build(target: &str, extra_params: &str) {
+    let mut args = vec!["build", "--target", target];
+    if !extra_params.is_empty() {
+        args.push(extra_params);
+    }
+
     let status = Command::new("cargo")
-        .args(&["build", "--target", target, "-vv"])
+        .args(&args)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
@@ -52,36 +62,52 @@ fn cargo_build(target: &str) {
     // honors the subprocess exit code
     if status.success() {
         exit(1)
-    } else {
-        exit(status.code().unwrap());
     }
+    exit(status.code().unwrap());
+}
+
+/// Displays usage message
+fn print_usage(opts: Options) {
+    let brief = "cargo-kubos is a helper utility for building \
+        Kubos/yotta based modules under Cargo.\nIt is \
+        used when building crates which either contain \
+        a yotta module or depend on one. \
+        \n\nUsage:\
+        \n\tcargo kubos [options]\
+        \n\tcargo kubos [options] -- [cargo options]";
+    print!("{}", opts.usage(&brief));
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let mut opts = Options::new();
-    opts.optflag("b", "build", "builds module");
-    opts.optopt("t", "target", "sets (kubos) build target", "NAME");
+
+    opts.optopt("t", "target", "sets (Kubos) build target", "NAME");
+    opts.optflag("h", "help", "Displays help");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(f) => panic!(f.to_string()),
     };
-    if matches.opt_present("b") {
+
+    // Collect extra parametrs
+    let extra_params = if !matches.free.is_empty() {
+        let mut extras = matches.free.clone();
+        // Remove `kubos` parameter
+        extras.remove(0);
+        extras.join(" ")
+    } else {
+        String::from("")
+    };
+
+    if matches.opt_present("h") {
+        print_usage(opts);
+    } else {
         let k_target = match matches.opt_str("t") {
             Some(t) => t,
             None => String::from("x86-linux-native"),
         };
         let c_target = target_converter(&k_target);
         env::set_var("CARGO_KUBOS_TARGET", &k_target);
-        cargo_build(&c_target);
-    } else {
-        println!("cargo-kubos is a helper utility for building");
-        println!("kubos/yotta based modules under Cargo. It is");
-        println!("primarily used when building crates which either");
-        println!("contain a yotta module or depend on one.");
-        println!("");
-        println!("    -b, --build   : builds current module");
-        println!("    -t, --target  : sets kubos target");
+        cargo_build(&c_target, &extra_params);
     }
 }
