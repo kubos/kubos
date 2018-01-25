a39c9849ec76d0add1ed6b24e34946201781bcd8
Ryan Plauche <ryan@kubos.co>
2018-01-25 10:26:45 -0600

    Automating docs generation and deployment. Created script to generate docs from within circleci container and script to separately upload them. Updated circleci container with docs tools.

---
+++ deploy_docs.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+latest_tag=`git tag --sort=-creatordate | head -n 1`
+
+echo "Uploading new docs.."
+git clone https://github.com/kubos/pm-tools
+./pm-tools/docs/upload_docs.sh $latest_tag ./html/
---
+++ prep_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+latest_tag=`git tag --sort=-creatordate | head -n 1`
+
+echo "Tag for docs: $latest_tag"
+
+# Place current docs tag into docs conf
+sed -i "s/0.0.0/$latest_tag/g" docs/conf.py
+
+echo "Generating new docs"
+./tools/gendocs.py --version $latest_tag
+
+chmod a+rw html/
