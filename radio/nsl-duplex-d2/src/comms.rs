c276f748a91f157edd5c091b68a80eb0915e495e
Dave Sims <davsims@gmail.com>
2018-02-10 12:59:31 -0600

    Add health status constant/command

--- radio/nsl-duplex-d2/src/comms.rs
+++ radio/nsl-duplex-d2/src/comms.rs
@@ -1,4 +1,5 @@
 pub const GET_UPLOADED_FILE_COUNT: &str = "GUGETUFC";
+pub const GET_MODEM_HEALTH_STATE: &str = "GUGETHS";
 pub const GET_UPLOADED_FILE: &str = "GUGET_UF";
 pub const RESP_HEADER: &str = "GU";
 
