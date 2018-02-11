993f6e5203c7433879972a33cd821c2d5c8c9671
Dave Sims <davsims@gmail.com>
2018-02-11 13:40:43 -0600

    Implement D2Message for SOHRecord

--- radio/nsl-duplex-d2/src/state_of_health_record.rs
+++ radio/nsl-duplex-d2/src/state_of_health_record.rs
@@ -1,19 +1,36 @@
+use ::*;
+use d2_message::{D2Message};
+
 #[derive(Default)]
 pub struct StateOfHealthRecord {
-   reset_count:                     [u8; 4], // (4 byte integer) Current epoch reset count, starts at 0, incremented for each power system reset, persistent over the life of the mission
-   current_time:                    [u8; 4], // (4 byte integer) Current time (seconds) from start of most recent reset
-   current_rssi:                    u8,      // (1 byte integer) Current RSSI (Received Signal Strength Indicator), 0 to 4
-   connection_status:               u8,      // (1 byte integer) Connection status, 0 (connected) or 1 (disconnected)
-   globalstar_gateway:              u8,      // (1 byte integer) Globalstar gateway connected to, proprietary ID, 0 to 255
-   last_contact_time:               [u8; 4], // (4 byte integer) Last contact time, seconds since latest reset
-   last_attempt_time:               [u8; 4], // (4 byte integer) Last attempt time, seconds since latest reset
-   call_attempts_since_reset:       [u8; 4], // (4 byte integer) Count of call attempts since latest reset
-   successful_connects_since_reset: [u8; 4], // (4 byte integer) Count of successful connects since latest reset
-   average_connection_duration:     [u8; 4], // (4 byte integer) Average connection duration (seconds)
-   connection_duration_std_dev:     [u8; 4]  // (4 byte integer) Connection duration standard deviation (seconds)
+    message: Vec<u8>,
+    pub reset_count:                     [u8; 4], // (4 byte integer) Current epoch reset count, starts at 0, incremented for each power system reset, persistent over the life of the mission
+    pub current_time:                    [u8; 4], // (4 byte integer) Current time (seconds) from start of most recent reset
+    pub current_rssi:                    u8,      // (1 byte integer) Current RSSI (Received Signal Strength Indicator), 0 to 4
+    pub connection_status:               u8,      // (1 byte integer) Connection status, 0 (connected) or 1 (disconnected)
+    pub globalstar_gateway:              u8,      // (1 byte integer) Globalstar gateway connected to, proprietary ID, 0 to 255
+    pub last_contact_time:               [u8; 4], // (4 byte integer) Last contact time, seconds since latest reset
+    pub last_attempt_time:               [u8; 4], // (4 byte integer) Last attempt time, seconds since latest reset
+    pub call_attempts_since_reset:       [u8; 4], // (4 byte integer) Count of call attempts since latest reset
+    pub successful_connects_since_reset: [u8; 4], // (4 byte integer) Count of successful connects since latest reset
+    pub average_connection_duration:     [u8; 4], // (4 byte integer) Average connection duration (seconds)
+    pub connection_duration_std_dev:     [u8; 4]  // (4 byte integer) Connection duration standard deviation (seconds)
+}
+
+impl D2Message for StateOfHealthRecord {
+	fn new(message: Vec<u8>) -> StateOfHealthRecord {
+		let mut state_of_health_record: StateOfHealthRecord = Default::default();
+		state_of_health_record.message = message;
+		state_of_health_record
+	}
+
+    fn message(&self) -> & Vec<u8> {
+        & self.message
+    }
 }
 
 impl StateOfHealthRecord {
+
     pub fn from_response(soh_response: Vec<u8>) -> StateOfHealthRecord {
         let mut state_of_health_record: StateOfHealthRecord = Default::default();
         state_of_health_record.reset_count.copy_from_slice(&soh_response[2..6]);
@@ -32,66 +49,31 @@ impl StateOfHealthRecord {
 }
 
 #[cfg(test)]
-mod tests {
+pub mod tests {
     use ::*;
     use state_of_health_record::*;
 
     #[test]
+    fn test_new() {
+        let soh_record = StateOfHealthRecord::new(soh_message());
+    }
+
+    #[test]
     fn test_reset_count() {
         assert_eq!(test_record().reset_count, [0,0,1,2]);
-    }
-
-    #[test]
-    fn test_current_time() {
         assert_eq!(test_record().current_time, [5,6,7,8]);
-    }
-
-    #[test]
-    fn test_current_rssi() {
         assert_eq!(test_record().current_rssi, 9);
-    }
-
-    #[test]
-    fn test_connection_status() {
         assert_eq!(test_record().connection_status, 3);
-    }
-
-    #[test]
-    fn test_globalstar_gateway() {
         assert_eq!(test_record().globalstar_gateway, 8);
-    }
-
-    #[test]
-    fn test_conact_time() {
         assert_eq!(test_record().last_contact_time, [0,7,0,7]);
-    }
-
-    #[test]
-    fn test_last_attempt_time() {
         assert_eq!(test_record().last_attempt_time, [1,0,1,0]);
-    }
-
-    #[test]
-    fn test_call_attempts_since_reset() {
         assert_eq!(test_record().call_attempts_since_reset, [3,3,3,3]);
-    }
-
-    #[test]
-    fn test_successful_connects_since_reset() {
         assert_eq!(test_record().successful_connects_since_reset, [1,1,9,9]);
-    }
-
-    #[test]
-    fn test_average_connection_duration() {
         assert_eq!(test_record().average_connection_duration, [7,6,5,4]);
-    }
-
-    #[test]
-    fn test_connection_duration_std_dev() {
         assert_eq!(test_record().connection_duration_std_dev, [2,2,2,2]);
     }
 
-    fn test_record() -> StateOfHealthRecord {
+    pub fn soh_message() -> Vec<u8> {
         let mut soh_message = Vec::<u8>::new();
         let reset_count = [0,0,1,2];
         let current_time = [5,6,7,8];
@@ -117,6 +99,10 @@ mod tests {
         soh_message.extend(successful_connects_since_reset.iter());
         soh_message.extend(average_connection_duration.iter());
         soh_message.extend(connection_duration_std_dev.iter());
-        StateOfHealthRecord::from_response(soh_message)
+        soh_message
+    }
+
+    fn test_record() -> StateOfHealthRecord {
+        StateOfHealthRecord::from_response(soh_message())
     }
 }
