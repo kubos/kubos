d1ee2b71d8c7f7a39a98fdbde2d9090424542586
Tim Caswell <tim@creationix.com>
2018-02-22 16:15:35 -0600

    Update comms-service to use new command API

--- radio/comms-service/src/main.rs
+++ radio/comms-service/src/main.rs
@@ -1,11 +1,12 @@
 extern crate nsl_duplex_d2;
 
+use nsl_duplex_d2::get_file_count;
 use nsl_duplex_d2::DuplexD2;
 use nsl_duplex_d2::SerialConnection;
 
 fn main() {
     let duplex = DuplexD2::new(Box::new(SerialConnection));
 
-    let count = duplex.get_uploaded_file_count().unwrap();
-    println!("Count {}", count);
+    let count = duplex.send_command(&get_file_count()).unwrap();
+    println!("Count {:?}", count);
 }
