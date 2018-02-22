6dc62d64d013d6109ffaaee8c7b006f7b939a835
Dave Sims <davsims@gmail.com>
2018-02-22 12:39:44 -0600

    Update & fix for I2C datatype test

--- hal/python-hal/i2c/i2c.py
+++ hal/python-hal/i2c/i2c.py
@@ -16,42 +16,43 @@ I2C_SLAVE=0x0703
 
 class I2C:
 
-    def __init__(self, bus):
+    def __init__(self, bus, filepath):
         """
         Retrieves the read/write file handle for the device
         """
-        self.filepath = "/dev/i2c-"+str(bus)
-    
+        # self.filepath = "/dev/i2c-"+str(bus)
+        self.filepath = filepath
+
     def write(self, device, data):
         """
         Sets the address for the device.
-        Formats the data and writes the data to the device. 
+        Formats the data and writes the data to the device.
         Input must be a string or a list.
         Returns True and the data (as written to the device) if successful
         """
-        
+
         with io.open(self.filepath, "r+b", buffering=0) as file:
 
             fcntl.ioctl(file, I2C_SLAVE, device)
-            
+
             if type(data) is list:
                 data = bytearray(data)
             elif type(data) is str:
                 pass
-            else: 
+            else:
                 raise TypeError('Invalid data format: '+str(type(data))+', must be string or list')
             file.write(data)
             return True,data
-    
+
     def read(self, device, count):
         """
-        Reads the specified number of bytes from the device. 
+        Reads the specified number of bytes from the device.
         """
         with io.open(self.filepath, "r+b", buffering=0) as file:
             fcntl.ioctl(file, I2C_SLAVE, device)
-            
+
             return file.read(count)
 
-        
 
-    
+
+
--- hal/python-hal/i2c/test_i2c.py
+++ hal/python-hal/i2c/test_i2c.py
@@ -11,14 +11,20 @@ Unit testing for the I2C library.
 import unittest
 import i2c
 import mock
+from contextlib import nested
 
 class TestI2C(unittest.TestCase):
-    
+
     def setUp(self):
-        self.i2cdevice = i2c.I2C(bus=1)
-    
+        self.i2cdevice = i2c.I2C(1, "/dev/123")
+
     def test_datatype(self):
-        self.assertRaises(TypeError,self.i2cdevice.write,123)
+        with nested(
+                mock.patch('io.open'),
+                mock.patch('fcntl.ioctl'),
+        ) as (mock_open, mock_ioctl):
+            with self.assertRaises(TypeError):
+                self.i2cdevice.write(1, 123)
 
 
 if __name__ == '__main__':
