c2fc8437876f399c46613859a78d3e46aaba8fac
Tim Caswell <tim@creationix.com>
2018-02-26 11:27:55 -0600

    Update comms-service to latest radio api

--- radio/comms-service/src/main.rs
+++ radio/comms-service/src/main.rs
@@ -1,16 +1,15 @@
 extern crate nsl_duplex_d2;
 
-use nsl_duplex_d2::{get_file, get_file_count};
 use nsl_duplex_d2::DuplexD2;
-use nsl_duplex_d2::SerialConnection;
+use nsl_duplex_d2::serial_connection;
 
 fn main() {
-    let duplex = DuplexD2::new(Box::new(SerialConnection));
+    let radio = DuplexD2::new(serial_connection());
 
-    let count = duplex.send_command(&get_file_count()).unwrap();
+    let count = radio.get_file_count().unwrap();
     println!("Count {:?}", count);
 
-    let file = duplex.send_command(&get_file()).unwrap();
+    let file = radio.get_file().unwrap();
     println!("File name: {}", file.name);
     println!("File data: {:?}", file.body);
 }
