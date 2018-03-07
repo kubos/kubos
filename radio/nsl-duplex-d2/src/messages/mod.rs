9207ce7737f6c9bc2d020d44ca5a2f25d73bea61
Tim Caswell <tim@creationix.com>
2018-03-07 11:39:28 -0600

    Add unit test for parse_ack

--- radio/nsl-duplex-d2/src/messages/mod.rs
+++ radio/nsl-duplex-d2/src/messages/mod.rs
@@ -41,10 +41,15 @@ pub fn parse_ack(input: &[u8]) -> IResult<&[u8], ()> {
 mod tests {
     use super::*;
     #[test]
-    fn it_parses() {
+    fn it_parses_u32() {
         assert_eq!(
             Ok((&b"extra"[..], 0x12345678)),
             parse_u32(b"GU\x12\x34\x56\x78extra")
         );
+    }
+
+    #[test]
+    fn it_parses_ack() {
+        assert_eq!(Ok((&b"extra"[..], ())), parse_ack(b"GU\x06extra"));
     }
 }
