d2e75c72517f5fbc63e05c1b6460d39a63e9c2e3
Tim Caswell <tim@creationix.com>
2018-03-07 12:11:29 -0600

    Update comms-service with renamed method

--- radio/comms-service/src/main.rs
+++ radio/comms-service/src/main.rs
@@ -6,10 +6,10 @@ use nsl_duplex_d2::serial_connection;
 fn main() {
     let radio = DuplexD2::new(serial_connection());
 
-    let count = radio.get_file_count().unwrap();
+    let count = radio.get_uploaded_file_count().unwrap();
     println!("Count {:?}", count);
 
-    let file = radio.get_file().unwrap();
+    let file = radio.get_uploaded_file().unwrap();
     println!("File name: {}", file.name);
     println!("File data: {:?}", file.body);
 }
