6896d74fc76c0dcfb3e92344360f0383a0291d37
Ryan Plauche <ryan@kubos.co>
2018-03-15 11:35:46 -0500

    Send and receive examples.

---
+++ radio/trxvu-radio-api/examples/receive.rs
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018 Kubos Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+extern crate trxvu_radio_api;
+
+use trxvu_radio_api::Trxvu;
+
+pub fn main() {
+    let radio = Trxvu::new();
+
+    println!("Receiving message");
+    let message: Vec<u8> = radio.read().unwrap();
+    println!("Received {:?}", message);
+}
