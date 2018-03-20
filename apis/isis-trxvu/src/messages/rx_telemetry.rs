4aff46d1e9986961f8dc82e83f236ade11cff9df
Ryan Plauche <ryan@kubos.co>
2018-03-20 18:06:49 -0500

    Added test for RxMessage.

--- apis/isis-trxvu/src/messages/rx_telemetry.rs
+++ apis/isis-trxvu/src/messages/rx_telemetry.rs
@@ -18,7 +18,7 @@ use ffi::*;
 use messages::convert::*;
 
 /// Struct for transmit telemetry
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct RxTelemetry {
     pub inst_doppler_offset: f32,
     pub supply_current: f32,
@@ -38,5 +38,31 @@ impl RxTelemetry {
             temp_power_amp: get_temperature(raw.temp_power_amp),
             inst_signal_strength: get_signal_strength(raw.inst_signal_strength),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let raw = RxTelemRaw {
+            inst_doppler_offset: 1,
+            supply_voltage: 1,
+            supply_current: 1,
+            temp_power_amp: 1,
+            temp_oscillator: 1,
+            inst_signal_strength: 1,
+        };
+        let result = RxTelemetry {
+            inst_doppler_offset: -22286.648,
+            supply_voltage: 0.00488,
+            supply_current: 0.16643964,
+            temp_power_amp: 195.52701,
+            temp_oscillator: 195.52701,
+            inst_signal_strength: -151.97,
+        };
+        assert_eq!(result, RxTelemetry::parse(&raw));
     }
 }
