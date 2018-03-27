86415b5c8f3f0c3d6f8de3bf0f043d14e3ef6c42
Tim Caswell <tim@creationix.com>
2018-03-27 15:39:44 -0500

    Use EXPOSE_PORTS to chose what remote ports are exposed locally

--- services/communication-service/main.lua
+++ services/communication-service/main.lua
@@ -73,25 +73,21 @@ local function make_receiver(dest)
   end)
 end
 
-local function on_message(err, data, addr)
-  assert(not err, err)
-  if not data or #data < 2 then return end
-  local source = addr.port
-  local dest = bor(
-    lshift(byte(data, 1), 8),
-    byte(data, 2)
-  )
-  data = sub(data, 3)
-  p('udp-req -> ' .. transport_name, {
-    source = source,
-    dest = dest,
-    len = #data
-  })
-  write {
-    source = source,
-    dest = dest,
-    data = data,
-  }
+local function make_sender(dest)
+  return wrap(function (err, data, addr)
+    assert(not err, err)
+    local source = addr.port
+    p('udp-req -> ' .. transport_name, {
+      source = source,
+      dest = dest,
+      len = #data
+    })
+    write {
+      source = source,
+      dest = dest,
+      data = data,
+    }
+  end)
 end
 
 wrap(function ()
@@ -101,16 +97,19 @@ wrap(function ()
   read = decoder(read, udp.decode)
   write = encoder(write, udp.encode)
 
-  -- Listen on known port for messages with target port embedded in first 2
-  -- bytes
-  local server = uv.new_udp()
-  local host = (getenv 'HOST') or '127.0.0.1'
-  local port = getenv 'PORT'
-  port = port and tonumber(port) or 5000
-  assert(server:bind(host, port))
-  print 'Communications service listening UDP:'
-  p(server:getsockname())
-  assert(server:recv_start(wrap(on_message)))
+  -- Expose remote ports locally if requested via EXPOSE_PORTS env variable.
+  local host = getenv("HOST") or "127.0.0.1"
+  local expose = getenv 'EXPOSE_PORTS'
+  if expose then
+    for dest in expose:gmatch("%d+") do
+      dest = tonumber(dest)
+      local server = uv.new_udp()
+      assert(server:bind(host, dest))
+      assert(server:recv_start(make_sender(dest)))
+      print 'Communications service forwarding UDP:'
+      p("repeater", server:getsockname())
+    end
+  end
 
   -- Listen for messages over the custom transport
   for message in read do
