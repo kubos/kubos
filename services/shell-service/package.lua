4232dcdf55f5ad3b9541589c75871025cbff8582
Tim Caswell <tim@creationix.com>
2018-04-02 12:20:02 -0500

    Update paths for luvi-tiny

--- services/shell-service/package.lua
+++ services/shell-service/package.lua
@@ -1,13 +1,13 @@
   return {
     name = "kubos/kubos-shell-service",
-    version = "0.0.3",
+    version = "0.0.4",
     description = "Service to provide access to linux files and processes remotely.",
     tags = { "kubos", "udp", "shell", "file", "process" },
     author = { name = "Tim Caswell", email = "tim@kubos.co" },
     homepage = "https://github.com/kubos/kubos",
     luvi = {
       flavor = "tiny",
-      inline = "#!/home/system/usr/local/bin/luvi-tiny --\n"
+      inline = "#!/home/system/usr/bin/luvi-tiny --\n"
     },
     dependencies = {
       "luvit/require",
