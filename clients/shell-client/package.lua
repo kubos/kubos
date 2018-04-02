b7130092ec6f3e3713a2926acc6072bea978b416
Tim Caswell <tim@creationix.com>
2018-04-02 14:05:50 -0500

    Add more license headers

--- clients/shell-client/package.lua
+++ clients/shell-client/package.lua
@@ -1,21 +1,36 @@
-  return {
-    name = "kubos/kubos-shell-client",
-    version = "0.0.5",
-    description = "Shell client to connect to remote shell service over custom transport.",
-    tags = { "kubos", "udp", "nat", "shell"},
-    author = { name = "Tim Caswell", email = "tim@kubos.co" },
-    homepage = "https://github.com/kubos/kubos",
-    luvi = {
-      flavor = "tiny",
-      inline = "#!/home/system/usr/bin/luvi-tiny --\n"
-    },
-    dependencies = {
-      "luvit/require",
-      "luvit/pretty-print",
-      "luvit/readline",
-      "creationix/cbor",
-    },
-    files = {
-      "**.lua",
-    }
+--[[
+Copyright (C) 2018 Kubos Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]
+return {
+  name = "kubos/kubos-shell-client",
+  version = "0.0.5",
+  description = "Shell client to connect to remote shell service over custom transport.",
+  tags = { "kubos", "udp", "nat", "shell"},
+  author = { name = "Tim Caswell", email = "tim@kubos.co" },
+  homepage = "https://github.com/kubos/kubos",
+  luvi = {
+    flavor = "tiny",
+    inline = "#!/home/system/usr/bin/luvi-tiny --\n"
+  },
+  dependencies = {
+    "luvit/require",
+    "luvit/pretty-print",
+    "luvit/readline",
+    "creationix/cbor",
+  },
+  files = {
+    "**.lua",
   }
+}
--- services/shell-service/package.lua
+++ services/shell-service/package.lua
@@ -1,21 +1,36 @@
-  return {
-    name = "kubos/kubos-shell-service",
-    version = "0.0.4",
-    description = "Service to provide access to linux files and processes remotely.",
-    tags = { "kubos", "udp", "shell", "file", "process" },
-    author = { name = "Tim Caswell", email = "tim@kubos.co" },
-    homepage = "https://github.com/kubos/kubos",
-    luvi = {
-      flavor = "tiny",
-      inline = "#!/home/system/usr/bin/luvi-tiny --\n"
-    },
-    dependencies = {
-      "luvit/require",
-      "luvit/pretty-print",
-      "creationix/cbor",
-    },
-    files = {
-      "**.lua",
-    },
-    license = "Apache 2.0"
-  }
+--[[
+Copyright (C) 2018 Kubos Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]
+return {
+  name = "kubos/kubos-shell-service",
+  version = "0.0.4",
+  description = "Service to provide access to linux files and processes remotely.",
+  tags = { "kubos", "udp", "shell", "file", "process" },
+  author = { name = "Tim Caswell", email = "tim@kubos.co" },
+  homepage = "https://github.com/kubos/kubos",
+  luvi = {
+    flavor = "tiny",
+    inline = "#!/home/system/usr/bin/luvi-tiny --\n"
+  },
+  dependencies = {
+    "luvit/require",
+    "luvit/pretty-print",
+    "creationix/cbor",
+  },
+  files = {
+    "**.lua",
+  },
+  license = "Apache 2.0"
+}
