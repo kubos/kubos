edc98459a8231f9e3c462f2e63278630807b9b3f
Tim Caswell <tim@creationix.com>
2018-04-02 12:15:46 -0500

    Use correct bin path

--- clients/udp-client/package.lua
+++ clients/udp-client/package.lua
@@ -1,13 +1,13 @@
   return {
     name = "kubos/kubos-udp-client",
-    version = "0.0.1",
+    version = "0.0.2",
     description = "UDP client for textual UDP services.",
     tags = { "kubos", "udp", "readline"},
     author = { name = "Tim Caswell", email = "tim@kubos.co" },
     homepage = "https://github.com/kubos/kubos",
     luvi = {
       flavor = "tiny",
-      inline = "#!/home/system/usr/local/bin/luvi-tiny --\n"
+      inline = "#!/home/system/usr/bin/luvi-tiny --\n"
     },
     dependencies = {
       "luvit/require",
