4670870c849f7f8ac98ed20be2ba67c8d492566c
Tim Caswell <tim@creationix.com>
2018-04-02 12:13:40 -0500

    Add simple udp client for testing textual services

---
+++ clients/udp-client/main.lua
@@ -0,0 +1,42 @@
+local uv = require 'uv'
+local dump = require('pretty-print').dump
+local Editor = require('readline').Editor
+
+local usage = [[
+Kubos UDP Client
+This is a tiny tool that provides a line interface to UDP services. It can talk
+to local services or remote services through the communications bridge.
+
+Usage: kubos-udp-client $service_port
+]]
+
+local port = args[1]
+port = port and tonumber(port)
+if not port then
+  print(usage)
+  return -1
+end
+local editor = Editor.new {}
+local prompt = 'udp:' .. port .. '> '
+
+local udp = uv.new_udp()
+
+assert(udp:bind('127.0.0.1', 0))
+
+local function on_line(err, line)
+  assert(not err, err)
+  udp:send(line, '127.0.0.1', port)
+  editor:readLine(prompt, on_line)
+end
+
+
+udp:recv_start(function (err, data, addr)
+  assert(not err, err)
+  if not data then return end
+  editor:insertAbove(dump(addr))
+  editor:insertAbove(data)
+end)
+
+editor:readLine(prompt, on_line)
+
+uv.run()
---
+++ clients/udp-client/package.lua
@@ -0,0 +1,20 @@
+  return {
+    name = "kubos/kubos-udp-client",
+    version = "0.0.1",
+    description = "UDP client for textual UDP services.",
+    tags = { "kubos", "udp", "readline"},
+    author = { name = "Tim Caswell", email = "tim@kubos.co" },
+    homepage = "https://github.com/kubos/kubos",
+    luvi = {
+      flavor = "tiny",
+      inline = "#!/home/system/usr/local/bin/luvi-tiny --\n"
+    },
+    dependencies = {
+      "luvit/require",
+      "luvit/pretty-print",
+      "luvit/readline",
+    },
+    files = {
+      "**.lua",
+    }
+  }
