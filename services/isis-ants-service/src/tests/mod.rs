9424493e09364b512f0880d5f2ad9ff1a7b37ae3
Catherine Garabedian <catherine@kubos.co>
2018-04-04 22:12:24 +0000

    Adding schema unit tests

---
+++ services/isis-ants-service/src/tests/mod.rs
@@ -0,0 +1,100 @@
+//
+// Copyright (C) 2018 Kubos Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use double;
+//use std::cell::RefCell;
+use isis_ants_api::*;
+use super::objects::*;
+
+macro_rules! mock_new {
+    () => (
+        MockAntS::new(
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+        Err(AntsError::ConfigError.into()),
+    )
+    )
+}
+
+mock_trait_no_default!(
+		MockAntS,
+		configure(KANTSController) -> AntSResult<()>,
+		reset() -> AntSResult<()>,
+		arm() -> AntSResult<()>,
+		disarm() -> AntSResult<()>,
+		deploy(KANTSAnt, bool, u8) -> AntSResult<()>,
+		auto_deploy(u8) -> AntSResult<()>,
+		cancel_deploy() -> AntSResult<()>,
+		get_deploy() -> AntSResult<DeployStatus>,
+		get_uptime() -> AntSResult<u32>,
+		get_system_telemetry() -> AntSResult<AntsTelemetry>,
+		get_activation_count(KANTSAnt) -> AntSResult<u8>,
+		get_activation_time(KANTSAnt) -> AntSResult<u16>,
+		watchdog_kick() -> AntSResult<()>,
+		watchdog_start() -> AntSResult<()>,
+		watchdog_stop() -> AntSResult<()>,
+		passthrough(Vec<u8>, Vec<u8>) -> AntSResult<()>
+	);
+
+impl IAntS for MockAntS {
+    fn new(
+        _bus: KI2CNum,
+        _primary: u8,
+        _secondary: u8,
+        _ant_count: u8,
+        _timeout: u32,
+    ) -> AntSResult<MockAntS> {
+        Ok(mock_new!())
+    }
+    mock_method!(configure(&self, config: KANTSController) -> AntSResult<()>);
+    mock_method!(reset(&self) -> AntSResult<()>);
+    mock_method!(arm(&self) -> AntSResult<()>);
+    mock_method!(disarm(&self) -> AntSResult<()>);
+    mock_method!(deploy(&self, antenna: KANTSAnt, force: bool, timeout: u8) -> AntSResult<()>);
+    mock_method!(auto_deploy(&self, timeout: u8) -> AntSResult<()>);
+    mock_method!(cancel_deploy(&self) -> AntSResult<()>);
+    mock_method!(get_deploy(&self) -> AntSResult<DeployStatus>);
+    mock_method!(get_uptime(&self) -> AntSResult<u32>);
+    mock_method!(get_system_telemetry(&self) -> AntSResult<AntsTelemetry>);
+    mock_method!(get_activation_count(&self, antenna: KANTSAnt) -> AntSResult<u8>);
+    mock_method!(get_activation_time(&self, antenna: KANTSAnt) -> AntSResult<u16>);
+    mock_method!(watchdog_kick(&self) -> AntSResult<()>);
+    mock_method!(watchdog_start(&self) -> AntSResult<()>);
+    mock_method!(watchdog_stop(&self) -> AntSResult<()>);
+    mock_method!(passthrough(&self, tx: &[u8], rx: &mut [u8]) -> AntSResult<()>, self, {
+            for (i, elem) in rx.iter_mut().enumerate () {
+                *elem = i as u8;
+            }
+            self.passthrough.call((tx.to_vec(), rx.to_vec()))
+        });
+}
+
+
+mod model;
+mod schema;
