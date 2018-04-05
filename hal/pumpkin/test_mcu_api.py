444cb7251174bebf27945c7733874a35d94534f6
jacoffey3 <jacoffey3@gmail.com>
2018-04-05 18:42:55 +0000

    added service framework from examples

---
+++ hal/pumpkin/test_mcu_api.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Kubos Corporation
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+"""
+API for interacting with all Pumpkin SupMCUs. 
+
+See Pumpkin SUPERNOVA Firmware Reference Manual Rev 3.5
+"""
