821da8191245959d2f9c1a884bb4f764a2347c09
Tim Caswell <tim@creationix.com>
2018-04-30 15:24:03 -0500

    Publish new versions of shell service and client to lit

--- services/shell-service/package.lua
+++ services/shell-service/package.lua
@@ -1,6 +1,6 @@
 return {
   name = "kubos/kubos-shell-service",
-  version = "0.0.6",
+  version = "1.0.0",
   description = "Service to provide access to linux files and processes remotely.",
   tags = { "kubos", "udp", "shell", "file", "process" },
   author = { name = "Tim Caswell", email = "tim@kubos.co" },
