d123aed30a0f23f0950cb988570ef022e67c98ed
Catherine Garabedian <catherine@kubos.co>
2018-05-14 19:57:10 +0000

    Converting Rust UART HAL to be multi-threaded compatible. Adding mock objects

--- hal/rust-hal/rust-uart/src/lib.rs
+++ hal/rust-hal/rust-uart/src/lib.rs
@@ -1,10 +1,35 @@
+//
+// Copyright (C) 2018 Kubos Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 #![deny(missing_docs)]
 
 //! A generalized HAL for communicating over serial ports
+#[macro_use]
+extern crate failure;
 extern crate serial;
+
+pub mod mock;
+#[cfg(test)]
+mod tests;
 
 use std::io::prelude::*;
 use serial::prelude::*;
+use std::time::Duration;
+use std::cell::RefCell;
+use std::error::Error;
 
 /// Wrapper for UART stream
 pub struct Connection {
@@ -12,8 +37,50 @@ pub struct Connection {
     pub stream: Box<Stream>,
 }
 
+/// Custom errors for UART actions
+#[derive(Fail, Display, Debug, Clone, PartialEq)]
+pub enum UartError {
+    /// Catch-all error case
+    #[display(fmt = "Generic Error")]
+    GenericError,
+    #[display(fmt = "Serial Error: {}", description)]
+    /// An error was thrown by the serial driver
+    SerialError {
+        /// The underlying error type
+        cause: serial::ErrorKind,
+        /// Error description
+        description: String,
+    },
+    #[display(fmt = "IO Error: {}", description)]
+    /// An I/O error was thrown by the kernel
+    IoError {
+        /// The underlying error type
+        cause: std::io::ErrorKind,
+        /// Error description
+        description: String,
+    },
+}
+
+impl From<std::io::Error> for UartError {
+    fn from(error: std::io::Error) -> Self {
+        UartError::IoError {
+            cause: error.kind(),
+            description: error.description().to_owned(),
+        }
+    }
+}
+
+impl From<serial::Error> for UartError {
+    fn from(error: serial::Error) -> Self {
+        UartError::SerialError {
+            cause: error.kind(),
+            description: error.description().to_owned(),
+        }
+    }
+}
+
 /// Errors that occur while reading from and writing to stream
-pub type UartResult<T> = Result<T, std::io::Error>;
+pub type UartResult<T> = Result<T, UartError>;
 
 impl Connection {
     /// Constructor to creation connection with provided stream
@@ -22,19 +89,14 @@ impl Connection {
     }
 
     /// Convenience constructor to create connection from bus path
-    pub fn from_path(bus: String) -> Connection {
-        Connection {
-            stream: Box::new(SerialStream {
-                bus,
-                settings: serial::PortSettings {
-                    baud_rate: serial::Baud115200,
-                    char_size: serial::Bits8,
-                    parity: serial::ParityNone,
-                    stop_bits: serial::Stop1,
-                    flow_control: serial::FlowNone,
-                },
-            }),
-        }
+    pub fn from_path(
+        bus: &str,
+        settings: serial::PortSettings,
+        timeout: Duration,
+    ) -> UartResult<Connection> {
+        Ok(Connection {
+            stream: Box::new(SerialStream::new(bus, settings, timeout)?),
+        })
     }
 
     /// Writes out raw bytes to the stream
@@ -43,42 +105,54 @@ impl Connection {
     }
 
     /// Reads messages upto specified length recieved on the bus
-    pub fn read(&self, len: usize) -> UartResult<Vec<u8>> {
-        self.stream.read(len)
+    pub fn read(&self, len: usize, timeout: Duration) -> UartResult<Vec<u8>> {
+        self.stream.read(len, timeout)
     }
 }
 
 /// This trait is used to represent streams and allows for mocking for api unit tests
-pub trait Stream {
+pub trait Stream: Send {
     /// Write raw bytes to stream
     fn write(&self, data: &[u8]) -> UartResult<()>;
 
     /// Read upto a specified amount of raw bytes from the stream
-    fn read(&self, len: usize) -> UartResult<Vec<u8>>;
+    fn read(&self, len: usize, timeout: Duration) -> UartResult<Vec<u8>>;
 }
 
 // This is the actual stream that data is tranferred over
 struct SerialStream {
-    bus: String,
-    settings: serial::PortSettings,
+    port: RefCell<serial::SystemPort>,
+    timeout: Duration,
+}
+
+impl SerialStream {
+    fn new(bus: &str, settings: serial::PortSettings, timeout: Duration) -> UartResult<Self> {
+        let mut port = serial::open(bus)?;
+
+        port.configure(&settings)?;
+
+        Ok(SerialStream {
+            port: RefCell::new(port),
+            timeout,
+        })
+    }
 }
 
 // Read and write implementations for the serial stream
 impl Stream for SerialStream {
     fn write(&self, data: &[u8]) -> UartResult<()> {
-        let mut port = serial::open(self.bus.as_str())?;
-
-        port.configure(&self.settings)?;
+        let mut port = self.port.try_borrow_mut().unwrap();
+        port.set_timeout(self.timeout)?;
 
         port.write_all(data)?;
 
         Ok(())
     }
 
-    fn read(&self, len: usize) -> UartResult<Vec<u8>> {
-        let mut port = serial::open(self.bus.as_str())?;
+    fn read(&self, len: usize, timeout: Duration) -> UartResult<Vec<u8>> {
+        let mut port = self.port.try_borrow_mut().unwrap();
 
-        port.configure(&self.settings)?;
+        port.set_timeout(timeout)?;
 
         let mut response: Vec<u8> = vec![0; len];
 
---
+++ hal/rust-hal/rust-uart/src/tests.rs
@@ -0,0 +1,244 @@
+//
+// Copyright (C) 2018 Kubos Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+use mock::*;
+use super::*;
+use std::time::Duration;
+
+#[test]
+fn mock_test() {
+    let mock = MockStream::default();
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    let packet: [u8; 40] = [0; 40];
+
+    assert_eq!(
+        connection.write(&packet).unwrap_err(),
+        UartError::GenericError
+    );
+}
+
+#[test]
+fn test_write_error_io() {
+    let mut mock = MockStream::default();
+
+    mock.write
+        .set_result(Err(UartError::from(std::io::Error::new(
+            std::io::ErrorKind::TimedOut,
+            "Operation timed out",
+        ))));
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    let packet: [u8; 40] = [0; 40];
+
+    assert_eq!(
+        connection.write(&packet).unwrap_err(),
+        UartError::IoError {
+            cause: std::io::ErrorKind::TimedOut,
+            description: "Operation timed out".to_owned(),
+        }
+    );
+}
+
+#[test]
+fn test_write_error_serial() {
+    let mut mock = MockStream::default();
+
+    mock.write
+        .set_result(Err(UartError::from(serial::Error::new(
+            serial::ErrorKind::NoDevice,
+            "Device unavailable",
+        ))));
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    let packet: [u8; 40] = [0; 40];
+
+    assert_eq!(
+        connection.write(&packet).unwrap_err(),
+        UartError::SerialError {
+            cause: serial::ErrorKind::NoDevice,
+            description: "Device unavailable".to_owned(),
+        }
+    );
+}
+
+#[test]
+#[should_panic]
+fn test_write_bad_input() {
+    let mut mock = MockStream::default();
+
+    mock.write.set_input([0, 1, 2, 3].to_vec());
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    let packet: [u8; 40] = [0; 40];
+
+    // This will fail under the covers because the passed input argument
+    // doesn't match what we said we were expecting
+    let _result = connection.write(&packet);
+}
+
+#[test]
+fn test_write_good() {
+    let mut mock = MockStream::default();
+
+    mock.write.set_input(vec![0, 1, 2, 3]);
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    let packet: [u8; 4] = [0, 1, 2, 3];
+
+    assert_eq!(connection.write(&packet), Ok(()));
+}
+
+#[test]
+fn test_read_error_io() {
+    let mut mock = MockStream::default();
+
+    mock.read
+        .set_result(Err(UartError::from(std::io::Error::new(
+            std::io::ErrorKind::TimedOut,
+            "Operation timed out",
+        ))));
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    assert_eq!(
+        connection.read(5, Duration::new(0, 0)).unwrap_err(),
+        UartError::IoError {
+            cause: std::io::ErrorKind::TimedOut,
+            description: "Operation timed out".to_owned(),
+        }
+    );
+}
+
+#[test]
+fn test_read_error_serial() {
+    let mut mock = MockStream::default();
+
+    mock.read.set_result(Err(UartError::from(serial::Error::new(
+        serial::ErrorKind::NoDevice,
+        "Device unavailable",
+    ))));
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    assert_eq!(
+        connection.read(5, Duration::new(0, 0)).unwrap_err(),
+        UartError::SerialError {
+            cause: serial::ErrorKind::NoDevice,
+            description: "Device unavailable".to_owned(),
+        }
+    );
+}
+
+#[test]
+fn test_read_bad_len() {
+    let mut mock = MockStream::default();
+
+    let expected = vec![0, 1, 2, 3, 4, 5];
+
+    mock.read.set_output(expected.clone());
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    // Explicitly setting the read to be one byte short
+    let result = connection.read(5, Duration::new(0, 0));
+
+    assert!(result.is_ok());
+    assert_ne!(result.unwrap(), expected);
+}
+
+#[test]
+fn test_read_bad_multi() {
+    let mut mock = MockStream::default();
+
+    let expected = vec![0, 1, 2, 3, 4, 5];
+
+    mock.read.set_output(expected.clone());
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    assert_eq!(
+        connection.read(3, Duration::new(0, 0)).unwrap(),
+        vec![0, 1, 2]
+    );
+    assert_eq!(
+        connection.read(4, Duration::new(0, 0)).unwrap_err(),
+        UartError::IoError {
+            cause: std::io::ErrorKind::TimedOut,
+            description: "Operation timed out".to_owned(),
+        }
+    );
+}
+
+#[test]
+fn test_read_good_single() {
+    let mut mock = MockStream::default();
+
+    let expected = vec![0, 1, 2, 3, 4, 5];
+
+    mock.read.set_output(expected.clone());
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    assert_eq!(connection.read(6, Duration::new(0, 0)).unwrap(), expected);
+}
+
+#[test]
+fn test_read_good_multi() {
+    let mut mock = MockStream::default();
+
+    let expected = vec![0, 1, 2, 3, 4, 5];
+
+    mock.read.set_output(expected.clone());
+
+    let connection = Connection {
+        stream: Box::new(mock),
+    };
+
+    assert_eq!(
+        connection.read(3, Duration::new(0, 0)).unwrap(),
+        vec![0, 1, 2]
+    );
+    assert_eq!(
+        connection.read(3, Duration::new(0, 0)).unwrap(),
+        vec![3, 4, 5]
+    );
+}
