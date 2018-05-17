6ae03f1b54d1d68bf94093a2b70f8f2e64ec5f22
Ryan Plauche <ryan@kubos.co>
2018-05-17 18:16:54 -0500

    Initial multithreading of udp transport.

--- services/comms-service/src/transports/udp.rs
+++ services/comms-service/src/transports/udp.rs
@@ -1,17 +1,31 @@
 use codecs::*;
+use std::collections::HashMap;
 use std::net::{SocketAddr, UdpSocket};
+use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
+use std::thread;
+use std::thread::JoinHandle;
 use std::time::Duration;
 
-use std::collections::HashMap;
+pub struct SocketInfo {
+    pub sender: Sender<udp::UdpData>,
+    pub handle: JoinHandle<()>,
+}
 
 pub struct Transport {
-    pub sockets: HashMap<u16, UdpSocket>,
+    sockets: HashMap<u16, SocketInfo>,
+    /// Used by the transport to receive data from the socket threads
+    receiver: Receiver<udp::UdpData>,
+    /// Used by the socket threads to send data to the transport
+    sender: Sender<udp::UdpData>,
 }
 
 impl Transport {
     pub fn new() -> Self {
+        let (tx, rx) = channel::<udp::UdpData>();
         Self {
             sockets: HashMap::new(),
+            receiver: rx,
+            sender: tx,
         }
     }
 
@@ -31,11 +45,42 @@ impl Transport {
             return Err(format!("Failed to set udp read timeout {:?}", e));
         }
 
-        self.sockets.insert(dest_port, socket);
+        let (socket_tx, socket_rx) = channel::<udp::UdpData>();
+        let transport_tx = self.sender.clone();
+        let handle = thread::spawn(move || loop {
+            loop {
+                let dest = SocketAddr::from(([127, 0, 0, 1], 7000));
+                let mut buffer = vec![0u8; 4096];
+                if let Ok((amount, _source)) = socket.recv_from(&mut buffer) {
+                    transport_tx
+                        .send(udp::UdpData {
+                            source: 7000,
+                            dest: 7000,
+                            data: buffer[0..amount].to_vec(),
+                            checksum: false,
+                        })
+                        .unwrap();
+                }
+
+                if let Ok(data) = socket_rx.recv() {
+                    if let Err(e) = socket.send_to(&data.data, &dest) {
+                        println!("Failed socket.send_to {:?}", e);
+                    }
+                }
+            }
+        });
+
+        let info = SocketInfo {
+            sender: socket_tx,
+            handle: handle,
+        };
+
+        self.sockets.insert(dest_port, info);
+
         Ok(())
     }
 
-    fn grab_socket(&mut self, dest_port: u16) -> Result<&UdpSocket, String> {
+    fn grab_socket(&mut self, dest_port: u16) -> Result<&SocketInfo, String> {
         if !self.sockets.contains_key(&dest_port) {
             self.init_socket(dest_port)?;
         };
@@ -47,28 +92,27 @@ impl Transport {
     }
 
     pub fn read(&mut self, dest_port: u16) -> Result<Option<udp::UdpData>, String> {
-        let socket = self.grab_socket(dest_port)?;
+        let _ = self.grab_socket(dest_port)?;
 
-        let mut buffer = vec![0u8; 4069];
         println!("-udp-recv-");
-        if let Ok((amount, _source)) = socket.recv_from(&mut buffer) {
-            return Ok(Some(udp::UdpData {
-                source: 7000,
-                dest: 7000,
-                data: buffer[0..amount].to_vec(),
-                checksum: false,
-            }));
+
+        match self.receiver.try_recv() {
+            Ok(data) => Ok(Some(data)),
+            Err(TryRecvError::Empty) => Ok(None),
+            Err(TryRecvError::Disconnected) => {
+                Err(format!("Socket channel {:?} disconnected", dest_port))
+            }
         }
-        return Ok(None);
     }
 
     pub fn write(&mut self, data: udp::UdpData, dest_port: u16) -> Result<(), String> {
         let socket = self.grab_socket(dest_port)?;
 
-        let dest = SocketAddr::from(([127, 0, 0, 1], data.dest));
-
-        if let Err(e) = socket.send_to(&data.data, &dest) {
-            return Err(format!("Failed to send udp packet {:?}", e));
+        if let Err(e) = socket.sender.send(data) {
+            return Err(format!(
+                "Failed to send udp channel msg {}:{:?}",
+                dest_port, e
+            ));
         }
 
         Ok(())
