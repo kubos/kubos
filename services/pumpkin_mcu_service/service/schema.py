077956583c8ceb8ec88f6167a3ca2a289b28f3e6
jacoffey3 <jacoffey3@gmail.com>
2018-05-17 16:22:20 +0000

    JSONString not supported...finding an alternative.

--- services/pumpkin_mcu_service/service/schema.py
+++ services/pumpkin_mcu_service/service/schema.py
@@ -9,7 +9,6 @@ Graphene schema setup to enable queries.
 """
 
 import graphene
-import json
 from models import *
 
 from pumpkin_mcu_api import mcu_api
@@ -27,27 +26,27 @@ class Query(graphene.ObjectType):
     Creates query endpoints exposed by graphene.
     """
     
-    mcuTelemetry = graphene.JSONString(
-        module=graphene.String(),
-        fields=graphene.List(graphene.String,default_value = ["all"]))
-    rawRead = graphene.JSONString(
+    rawRead = graphene.String(
         module=graphene.String(),
         count=graphene.Int())
         
-    def resolve_mcuTelemetry(self, info, module, fields):
-        """
-        Handles request for subsystem query.
-        """
-        if module not in MODULES:
-            raise KeyError('Module not configured',module)
-        address = MODULES[module]['address']
-        if address == 0:
-            raise ValueError('Module not present',module)
-        fields = map(str,fields)
-        mcu = mcu_api.MCU(address = address)
-        out = mcu.get_module_telemetry(module = module,fields = fields)
-        
-        return json.dumps(out)
+    # def resolve_mcuTelemetry(self, info, module, fields):
+    #     """
+    #     Handles request for subsystem query.
+    #     """
+    #     if module not in MODULES:
+    #         raise KeyError('Module not configured',module)
+    #     address = MODULES[module]['address']
+    #     if address == 0:
+    #         raise ValueError('Module not present',module)
+    #     fields = map(str,fields)
+    #     mcu = mcu_api.MCU(address = address)
+    #     out = mcu.get_module_telemetry(module = module,fields = fields)
+    #     # out = {"field1":{"timestamp": 152349502.1,"data":12345},
+    #     #     "field2": {"timestamp": 152349502.1,"data":"stuffandthings"},
+    #     #     "field3": {"timestamp": 152349502.1,"data":1245234.212415},
+    #     #     "field4": {"timestamp": 152349502.1,"data":"\x12\x23\x34\x45\x56\x67\x78\x89\x0a"}}
+    #     return out
     
     def resolve_rawRead(self, info, module, count):
         """
@@ -59,9 +58,8 @@ class Query(graphene.ObjectType):
         if address == 0:
             raise ValueError('Module not present',module)
         mcu = mcu_api.MCU(address = address)
-        out = mcu.read(count = count)
-        
-        return json.dumps(out)
+        out = mcu.
+        return 
         
 
 
