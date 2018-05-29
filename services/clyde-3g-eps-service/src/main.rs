abb9961d02c025acbae91db4a0038efbc7785e75
Ryan Plauche <ryan@kubos.co>
2018-05-29 17:44:12 -0500

    Adding documentation.

--- services/clyde-3g-eps-service/src/main.rs
+++ services/clyde-3g-eps-service/src/main.rs
@@ -14,8 +14,71 @@
 // limitations under the License.
 //
 
-// #![deny(missing_docs)]
-// #![deny(warnings)]
+//! Service for interacting with a ClydeSpace 3G EPS
+//!
+//! # Configuration
+//!
+//! The service can be configured in the `/home/system/etc/config.toml` with the following fields:
+//!
+//! ```toml
+//! [clyde-3g-eps-service]
+//! bus = "i2c-1"
+//!
+//! [clyde-3g-eps-service.addr]
+//! ip = "127.0.0.1"
+//! port = 8089
+//! ```
+//!
+//! Where `bus` specifies the I2c bus the eps is on, `ip` specifies the
+//! service's IP address, and `port` specifies the port on which the service will be
+//! listening for UDP packets.
+//!
+//! # Running the Service
+//!
+//! The service should be started automatically by its init script, but may also be started manually:
+//!
+//! ```bash
+//! $ clyde-3g-eps-service
+//! Listening on: 127.0.0.1:8089
+//! ```
+//!
+//! If no config file is specified then the service will look at `/home/system/etc/config.toml`.
+//! An alternative config file may be specified on the command line at run time:
+//!
+//! ```bash
+//! $ clyde-3g-eps-service -c config.toml
+//! ```
+//!
+//! # Panics
+//!
+//! Attempts to grab bus from Configuration and will `panic!` if not found.
+//!
+//! # GraphQL Schema
+//!
+//! ## Queries
+//!
+//! `type Query { ping: String }`
+//!
+//! `type Query { version: `[Data](models/version/struct.Data.html)` }` - Calls [Eps::get_version_info](../clyde_3g_eps_api/eps/struct.Eps.html#method.get_version_info)
+//!
+//! `type Query { resetTelemetry(telem_type: `[Type](models/reset_telemetry/enum.Type.html)`): `[Data](models/reset_telemetry/struct.Data.html)` }` - Calls [Eps::get_reset_telemetry](../clyde_3g_eps_api/eps/struct.Eps.html#method.get_reset_telemetry)
+//!
+//! `type Query { motherboardTelemetry(telem_type: `[Type](models/motherboard_telemetry/enum.Type.html)`): i32 }` - Calls [Eps::get_motherboard_telemetry](../clyde_3g_eps_api/eps/struct.Eps.html#method.get_motherboard_telemetry)
+//!
+//! `type Query { daughterboardTelemetry(telem_type: `[Type](models/daughterboard_telemetry/enum.Type.html)`): i32 }` - Calls [Eps::get_daughterboard_telemetry](../clyde_3g_eps_api/eps/struct.Eps.html#method.get_daughterboard_telemetry)
+//!
+//! `type Query { watchdogPeriod(): i32 }` - Calls [Eps::get_comms_watchdog_period](../clyde_3g_eps_api/eps/struct.Eps.html#method.get_comms_watchdog_period)
+//!
+//! ## Mutations
+//!
+//! `type Mutation { `manualReset` }` - Calls [Eps::manual_reset](../clyde_3g_eps_api/eps/struct.Eps.html#method.manual_reset)
+//!
+//! `type Mutation { resetWatchdog }` - Calls [Eps::reset_comms_watchdog](../clyde_3g_eps_api/eps/struct.Eps.html#method.reset_comms_watchdog)
+//!
+//! `type Mutation { setWatchdogPeriod(period: i32) }` - Calls [Eps::set_comms_watchdog_period](../clyde_3g_eps_api/eps/struct.Eps.html#method.set_comms_watchdog_period)
+
+#![deny(missing_docs)]
+#![deny(warnings)]
 
 extern crate clyde_3g_eps_api;
 extern crate eps_api;
@@ -25,8 +88,8 @@ extern crate i2c_hal;
 extern crate juniper;
 extern crate kubos_service;
 
-mod models;
-mod schema;
+pub mod models;
+pub mod schema;
 
 use kubos_service::{Config, Service};
 use models::subsystem::Subsystem;
