7c0b260536295e84d25dcedae2608b56c1964fbc
jacoffey3 <jacoffey3@gmail.com>
2018-05-30 13:30:01 +0000

    integrated udp python service into a pumpkin MCU service

---
+++ services/pumpkin-mcu-service-udp/service.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Kubos Corporation
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+"""
+Boilerplate main for service application.
+"""
+
+from service import schema
+
+from kubos_service import http_service
+from kubos_service import udp_service
+from kubos_service.config import Config
+
+c = Config("example-service")
+
+# Use this to start an http service
+#http_service.start(c, schema.schema)
+
+# Use this to start a udp service
+udp_service.start(c, schema)
---
+++ services/pumpkin-mcu-service-udp/service/schema.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Kubos Corporation
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+"""
+Graphene schema setup to enable queries.
+"""
+
+import graphene
+from models import *
+import json
+
+from pumpkin_mcu_api import mcu_api
+
+# Get what modules are present from config file
+MODULES = mcu_api.CONFIG_DATA['modules']
+
+class Query(graphene.ObjectType):
+    """
+    Creates query endpoints exposed by graphene.
+    """
+    moduleList = graphene.JSONString()
+    fieldList = graphene.List(graphene.String,module=graphene.String())
+    read = graphene.String(
+        module=graphene.String(),
+        count=graphene.Int())
+    mcuTelemetry = graphene.JSONString(
+        module=graphene.String(),
+        fields=graphene.List(graphene.String,default_value = ["all"]))
+    
+    def resolve_moduleList(self, info):
+        return json.dumps(MODULES)
+        
+    def resolve_fieldList(self, info, module):
+        if module not in MODULES:
+            raise KeyError('Module not configured',module)
+        address = MODULES[module]['address']
+        if address == 0:
+            raise ValueError('Module not present',module)
+        telemetry = mcu_api.CONFIG_DATA['telemetry']
+        fields = []
+        for field in telemetry["supervisor"]:
+            fields.append(field)
+        for field in telemetry[module]:
+            fields.append(field)
+        return fields
+    
+    def resolve_read(self, info, module, count):
+        """
+        Reads number of bytes from the specified MCU
+        """
+        if module not in MODULES:
+            raise KeyError('Module not configured',module)
+        address = MODULES[module]['address']
+        if address == 0:
+            raise ValueError('Module not present',module)
+        mcu = mcu_api.MCU(address = address)
+        bin_data = mcu.read(count = count)
+        
+        return bin_data.encode("hex")
+    
+    def resolve_mcuTelemetry(self, info, module, fields):
+        """
+        Handles request for subsystem query.
+        """
+        if module not in MODULES:
+            raise KeyError('Module not configured',module)
+        address = MODULES[module]['address']
+        if address == 0:
+            raise ValueError('Module not present',module)
+        fields = map(str,fields)
+        mcu = mcu_api.MCU(address = address)
+        out = mcu.read_telemetry(module = module,fields = fields)
+        return json.dumps(out)
+
+class Passthrough(graphene.Mutation):
+    """
+    Creates mutation for Passthrough Module Commanding
+    """
+
+    class Arguments:
+        module = graphene.String()
+        command = graphene.String()
+        
+    Output = CommandStatus
+
+    def mutate(self, info, module, command):
+        """
+        Handles passthrough commands to the Pumpkin MCU modules. 
+        """
+        if module not in MODULES:
+            raise KeyError('Module not configured',module)
+        address = MODULES[module]['address']
+        if address == 0:
+            raise KeyError('Module not present',module)
+        if type(command) == unicode: command = str(command)
+        mcu = mcu_api.MCU(address = address)
+        out = mcu.write(command)
+        
+        commandStatus = CommandStatus(status = out[0], command = out[1])
+        
+        return commandStatus
+
+
+class Mutation(graphene.ObjectType):
+    """
+    Creates mutation endpoints exposed by graphene.
+    """
+
+    passthrough = Passthrough.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
