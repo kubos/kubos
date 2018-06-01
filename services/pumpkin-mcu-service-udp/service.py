bc146dce73e1c465134e671653894411fbfc441d
jacoffey3 <jacoffey3@gmail.com>
2018-06-01 15:15:01 +0000

    changed name so it's not example service anymore

--- services/pumpkin-mcu-service-udp/service.py
+++ services/pumpkin-mcu-service-udp/service.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-# Copyright 2017 Kubos Corporation
+# Copyright 2018 Kubos Corporation
 # Licensed under the Apache License, Version 2.0
 # See LICENSE file for details.
 
 """
-Boilerplate main for service application.
+main for Pumpkin MCU service
 """
 
 from service import schema
@@ -13,7 +13,7 @@ from service import schema
 from kubos_service import udp_service
 from kubos_service.config import Config
 
-c = Config("example-service")
+c = Config("pumpkin-mcu-service")
 
 # Set which modules are present and their addresses. 
 schema.MODULES = c.raw['modules']
