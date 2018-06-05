52caace5014f808b31705638c4a96c3247f2597a
jacoffey3 <jacoffey3@gmail.com>
2018-06-05 13:35:40 -0400

    added python package files to gitignore and finished tests for mcu write and read

--- apis/pumpkin_mcu_api/test_mcu_api.py
+++ apis/pumpkin_mcu_api/test_mcu_api.py
@@ -10,3 +10,37 @@ API for interacting with all Pumpkin SupMCUs.
 Unit test module for the pumpkin mcu api
 """
 
+import unittest
+import mcu_api
+import mock
+
+class TestMCUAPI(unittest.TestCase):
+
+	def setUp(self):
+		self.mcu = mcu_api.MCU(address = 0x20)
+
+	def test_command_type(self):
+		with self.assertRaises(TypeError):
+			bad_command = 23 # Not a string
+			self.mcu.write(command = bad_command)
+
+	def test_stopbyte_appending(self):
+		fake_command = "SUP:LED ON"
+		with mock.patch('i2c.I2C.write') as mock_i2cwrite:
+			self.mcu.write(command = fake_command)
+			mock_i2cwrite.assert_called_with(
+				data = fake_command + '\x0a',
+				device = self.mcu.address)
+
+	def test_read(self):
+		read_count = 20
+		with mock.patch('i2c.I2C.read') as mock_i2cread:
+			self.mcu.read(count = read_count)
+			mock_i2cread.assert_called_with(
+				device = self.mcu.address,
+				count = read_count)
+
+	
+
+if __name__ == '__main__':
+    unittest.main()
