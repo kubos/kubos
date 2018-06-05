cb783da6b476b273ec61f733ff02e274ca64d7e0
Jesse Coffey <jessecoffey@jac-spacebook.local>
2018-06-05 12:15:25 -0400

    test file for pumpkin mcu api

---
+++ apis/pumpkin_mcu_api/test_mcu_api.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Kubos Corporation
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+"""
+API for interacting with all Pumpkin SupMCUs. 
+
+Unit test module for the pumpkin mcu api
+"""
+
