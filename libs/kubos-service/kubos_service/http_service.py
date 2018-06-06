80fc9db358eef412d2903987ded71f3b09ec7bad
Ryan Plauche <ryan@kubos.co>
2018-06-06 16:17:01 -0500

    Python package for building services.

---
+++ libs/kubos-service/kubos_service/http_service.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Kubos Corporation
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+
+"""
+Wrapper for creating a HTTP based Kubos service
+"""
+
+from flask import Flask
+from flask_graphql import GraphQLView
+
+def start(config, schema, context={}):
+    """
+    Creates flask based graphql and graphiql endpoints
+    """
+
+    app = Flask(__name__)
+    app.debug = True
+
+    app.add_url_rule(
+        '/',
+        view_func=GraphQLView.as_view(
+            'graphql',
+            schema=schema,
+            context=context
+            graphiql=False
+        )
+    )
+
+    app.add_url_rule(
+        '/graphiql',
+        view_func=GraphQLView.as_view(
+            'graphiql',
+            schema=schema,
+            context=context
+            graphiql=True
+        )
+    )
+
+    app.run(config.ip, config.port)
