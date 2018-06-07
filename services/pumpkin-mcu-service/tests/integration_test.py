f74e73bed86dc7c16d48bac533e48f164723d1ef
jacoffey3 <jacoffey3@gmail.com>
2018-06-07 10:34:24 -0400

    Added testing section to readme, reformatted the readme, and added error reporting to the integration test

--- services/pumpkin-mcu-service/tests/integration_test.py
+++ services/pumpkin-mcu-service/tests/integration_test.py
@@ -19,6 +19,7 @@ print "\n###################################"
 print "Service and Test Port Configuration"
 c = Config("pumpkin-mcu-service")
 testing_port = c.port + 1000
+ERRORS = {}
 
 print "Service IP:", c.ip
 print "Service port:", c.port
@@ -31,30 +32,40 @@ sock.bind((c.ip, testing_port))
 print "\n###########################"
 print "Query Modules and Addresses"
 query = 'query {moduleList}'
-print "\nquery:", query
+print "\nquery: " + query
 sock.sendto(query, (c.ip, c.port))
 raw_modules, addr = sock.recvfrom(1024)
 print raw_modules
 
 # turn modules into a proper dictionary
-raw_modules2 = json.loads(raw_modules)['msg']['moduleList']
-modules = json.loads(raw_modules2)
+dict_result = json.loads(raw_modules)
+modules = json.loads(dict_result['msg']['moduleList'])
 
-print "\n#################"
-print "fieldList queries"
+print "\n#################################"
+print "fieldList queries for all modules"
 for module in modules:
-    query = 'query {fieldList(module: "'+str(module)+'")}'
-    print "\nquery:", query
+    query = 'query {fieldList(module: "' + module + '")}'
+    print "\nquery: " + query
     sock.sendto(query, (c.ip, c.port))
     fieldList, addr = sock.recvfrom(1024)
-    print 'fieldList: ' + str(fieldList)
+    print 'fieldList: ' + fieldList
+    dict_result_fieldList = json.loads(fieldList)
+    if dict_result_fieldList['errs'] != None:
+        ERRORS.update({query: dict_result_fieldList['errs']})
 
 
-print "\n####################"
-print "mcuTelemetry queries"
+print "\n####################################"
+print "mcuTelemetry queries for all modules"
 for module in modules:
-    query = 'query {mcuTelemetry(module: "'+str(module)+'")}'
+    query = 'query {mcuTelemetry(module: "' + module + '")}'
     print "\nquery:", query
     sock.sendto(query, (c.ip, c.port))
     mcuTelemetry, addr = sock.recvfrom(1024)
-    print 'All fields for mcuTelemetry: '+str(mcuTelemetry)
+    print 'All fields for mcuTelemetry: ' + mcuTelemetry
+    dict_result_mcuTelemetry = json.loads(mcuTelemetry)
+    if dict_result_mcuTelemetry['errs'] != None:
+        ERRORS.update({query: dict_result_mcuTelemetry['errs']})
+
+print "\n############"
+print "Errors"
+print ERRORS
