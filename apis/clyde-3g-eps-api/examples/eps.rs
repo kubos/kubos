3b56aeba2dda8c6039ffbbb417a9ea2cc9dbdd47
Ryan Plauche <ryan@kubos.co>
2018-06-08 15:01:46 -0500

    Fixing example to dump all telemetry.

--- apis/clyde-3g-eps-api/examples/eps.rs
+++ apis/clyde-3g-eps-api/examples/eps.rs
@@ -38,8 +38,8 @@ macro_rules! dump_mother_telem {
     ) => {
         $(
             print_result!(
-                format!("{:?}", MotherboardTelemetry::$type),
-                $eps.get_motherboard_telemetry(MotherboardTelemetry::$type)
+                format!("{:?}", MotherboardTelemetry::Type::$type),
+                $eps.get_motherboard_telemetry(MotherboardTelemetry::Type::$type)
             );
             thread::sleep(Duration::from_millis(100));
         )+
@@ -53,8 +53,8 @@ macro_rules! dump_daughter_telem {
     ) => {
         $(
             print_result!(
-                format!("{:?}", DaughterboardTelemetry::$type),
-                $eps.get_daughterboard_telemetry(DaughterboardTelemetry::$type)
+                format!("{:?}", DaughterboardTelemetry::Type::$type),
+                $eps.get_daughterboard_telemetry(DaughterboardTelemetry::Type::$type)
             );
             thread::sleep(Duration::from_millis(100));
         )+
@@ -68,8 +68,8 @@ macro_rules! dump_reset_telem {
     ) => {
         $(
             print_result!(
-                format!("{:?}", ResetTelemetry::$type),
-                $eps.get_reset_telemetry(ResetTelemetry::$type)
+                format!("{:?}", ResetTelemetry::Type::$type),
+                $eps.get_reset_telemetry(ResetTelemetry::Type::$type)
             );
             thread::sleep(Duration::from_millis(100));
         )+
@@ -79,8 +79,113 @@ macro_rules! dump_reset_telem {
 pub fn main() {
     let eps = Eps::new(Connection::from_path("/dev/i2c-1", 0x2B));
 
-    loop {
-        eps.get_version_info();
-        thread::sleep(Duration::from_millis(50000));
-    }
+    print_result!("Version Info", eps.get_version_info());
+    thread::sleep(Duration::from_millis(100));
+
+    dump_mother_telem!(
+        eps,
+        VoltageFeedingBcr1,
+        CurrentBcr1Sa1a,
+        CurrentBcr1Sa1b,
+        ArrayTempSa1a,
+        ArrayTempSa1b,
+        SunDetectorSa1a,
+        SunDetectorSa1b,
+        VoltageFeedingBcr2,
+        CurrentBcr2Sa2a,
+        CurrentBcr2Sa2b,
+        ArrayTempSa2a,
+        ArrayTempSa2b,
+        SunDetectorSa2a,
+        SunDetectorSa2b,
+        VoltageFeedingBcr3,
+        CurrentBcr3Sa3a,
+        CurrentBcr3Sa3b,
+        ArrayTempSa3a,
+        ArrayTempSa3b,
+        SunDetectorSa3a,
+        SunDetectorSa3b,
+        BcrOutputCurrent,
+        BcrOutputVoltage,
+        CurrentDraw3V3,
+        CurrentDraw5V,
+        OutputCurrent12V,
+        OutputVoltage12V,
+        OutputCurrentBattery,
+        OutputVoltageBattery,
+        OutputCurrent5v,
+        OutputVoltage5v,
+        OutputCurrent33v,
+        OutputVoltage33v,
+        OutputVoltageSwitch1,
+        OutputCurrentSwitch1,
+        OutputVoltageSwitch2,
+        OutputCurrentSwitch2,
+        OutputVoltageSwitch3,
+        OutputCurrentSwitch3,
+        OutputVoltageSwitch4,
+        OutputCurrentSwitch4,
+        OutputVoltageSwitch5,
+        OutputCurrentSwitch5,
+        OutputVoltageSwitch6,
+        OutputCurrentSwitch6,
+        OutputVoltageSwitch7,
+        OutputCurrentSwitch7,
+        OutputVoltageSwitch8,
+        OutputCurrentSwitch8,
+        OutputVoltageSwitch9,
+        OutputCurrentSwitch9,
+        OutputVoltageSwitch10,
+        OutputCurrentSwitch10,
+        BoardTemperature,
+    );
+
+    dump_daughter_telem!(
+        eps,
+        VoltageFeedingBcr4,
+        CurrentBcr4Sa4a,
+        CurrentBcr4Sa4b,
+        ArrayTempSa4a,
+        ArrayTempSa4b,
+        SunDetectorSa4a,
+        SunDetectorSa4b,
+        VoltageFeedingBcr5,
+        CurrentBcr5Sa5a,
+        CurrentBcr5Sa5b,
+        ArrayTempSa5a,
+        ArrayTempSa5b,
+        SunDetectorSa5a,
+        SunDetectorSa5b,
+        VoltageFeedingBcr6,
+        CurrentBcr6Sa6a,
+        CurrentBcr6Sa6b,
+        ArrayTempSa6a,
+        ArrayTempSa6b,
+        SunDetectorSa6a,
+        SunDetectorSa6b,
+        VoltageFeedingBcr7,
+        CurrentBcr7Sa7a,
+        CurrentBcr7Sa7b,
+        ArrayTempSa7a,
+        ArrayTempSa7b,
+        SunDetectorSa7a,
+        SunDetectorSa7b,
+        VoltageFeedingBcr8,
+        CurrentBcr8Sa8a,
+        CurrentBcr8Sa8b,
+        ArrayTempSa8a,
+        ArrayTempSa8b,
+        SunDetectorSa8a,
+        SunDetectorSa8b,
+        VoltageFeedingBcr9,
+        CurrentBcr9Sa9a,
+        CurrentBcr9Sa9b,
+        ArrayTempSa9a,
+        ArrayTempSa9b,
+        SunDetectorSa9a,
+        SunDetectorSa9b,
+        BoardTemperature,
+    );
+
+    dump_reset_telem!(eps, BrownOut, AutomaticSoftware, Manual, Watchdog,);
 }
