474b62e83d9c8d84d955b7e554971711a76b6ff3
jacoffey3 <jacoffey3@gmail.com>
2018-06-12 09:32:11 -0400

    Better error reporting. All output goes to the main terminal as well as the results files.

--- test/test_suite/integration_test.py
+++ test/test_suite/integration_test.py
@@ -10,7 +10,7 @@ Runs Integration tests on all services included in service config file.
 Records results in individual files named: {servicenname}__results.txt
 
 Testing configuration toml is set up so that the system level config.toml
-can be used if a test_command field is added to each service within that 
+can be used if a test_command field is added to each service within that
 file.
 """
 
@@ -19,7 +19,6 @@ import toml
 
 DEFAULT_RESULT_PATH = "results/"
 TEST_CONFIG_PATH = "testing_config.toml"
-RESULTS_FILE_APPEND = "__results"
 
 
 class KubosServiceTest:
@@ -34,36 +33,34 @@ class KubosServiceTest:
     def runIntegrationTests(self):
         for service in self.test_config:
             # Create and open results file
-            service_result_file = open(
-                self.result_path+service+RESULTS_FILE_APPEND, "w+")
-            try:
+            filename = self.result_path + service
+            with open(filename, "w+") as service_result_file:
+                print "Running test for: ", service
                 # Store service config
                 service_config = self.test_config[service]
                 # Check that there is a command for that service
                 if "test_command" in service_config:
-                    # Run test and store results in file
-                    # Shell must be True to allow all full command line access
-                    # Stores both stdout and stderr to the file
-                    subprocess.call(service_config['test_command'],
-                                    shell=True,
-                                    stdout=service_result_file,
-                                    stderr=subprocess.STDOUT)
+                    try:
+                        output = subprocess.check_output(
+                            service_config['test_command'],
+                            stderr=subprocess.STDOUT,
+                            shell=True,
+                            universal_newlines=True)
+                    except subprocess.CalledProcessError as exc:
+                        # Subprocess encountered an error
+                        print("Status : FAIL", exc.returncode, exc.output)
+                        service_result_file.write("Status : FAIL\n")
+                        service_result_file.write(exc.output)
+                    else:
+                        # Test ran successfully
+                        print("Output: \n{}\n".format(output))
+                        service_result_file.write(
+                            "Output: \n{}\n".format(output))
                 else:
-                    # Note the lack of a test command in the results file
-                    service_result_file.write(
-                        "Service: "+service + " has no test_command\n")
-            except Exception as e:
-                # Log any errors encountered during testing in results file.
-                # These are only errors that occur if the test fails to run.
-                # Errors that are detected by testing are configured in each
-                # service's test.
-                service_result_file.write(
-                    "Service: "+service +
-                    "\n Failed to test with error: "+str(e))
-                print e
-            finally:
-                # Ensures the file gets closed
-                service_result_file.close()
+                    # Note the lack of a test command
+                    print("Status : FAIL", "No test_command field")
+                    service_result_file.write("Status : FAIL\n")
+                    service_result_file.write("No test_command field\n")
 
 
 if __name__ == '__main__':
