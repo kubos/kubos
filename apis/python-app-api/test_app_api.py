2c29880e19a13524da86a19035482e275b15eb38
jacoffey3 <jacoffey3@gmail.com>
2018-07-13 12:32:57 -0400

    Finished test suite and added a simple readme

--- apis/python-app-api/test_app_api.py
+++ apis/python-app-api/test_app_api.py
@@ -11,14 +11,13 @@ Unit testing for the I2C library.
 import app_api
 import unittest
 import mock
-from contextlib import nested
+import json
 
 
 class TestAppAPI(unittest.TestCase):
 
     def setUp(self):
-        self.api = app_api.Services(
-            config_filepath="test_config.toml")
+        self.api = app_api.Services("test_config.toml")
 
     def test_query_servicetype(self):
         bad_service = 1
@@ -32,10 +31,57 @@ class TestAppAPI(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.api.query(service=service, query=bad_query)
 
-    def test_query_call(self):
+    def test_query(self):
         service = "test-service"
         query = "test query"
-        self.assertEqual(1, 1)
+        ip = "0.0.0.0"
+        port = 8000
+        query_response = '{"errs":[],"msg":"test data"}'
+        timeout = 0.03
+        with mock.patch('socket.socket') as mock_sock:
+            mock_sock.return_value.recv.return_value = query_response
+            self.api.query(service=service, query=query, timeout=timeout)
+            mock_sock.return_value.sendto.assert_called_with(query, (ip, port))
+
+    def test_timeout(self):
+        service = "test-service"
+        query = "test query"
+        query_response = '{"errs":[],"msg":"test data"}'
+        timeout = 0.03
+        with mock.patch('socket.socket') as mock_sock:
+            mock_sock.return_value.recv.return_value = query_response
+            self.api.query(service=service, query=query, timeout=timeout)
+            mock_sock.return_value.settimeout.assert_called_with(timeout)
+
+    def test_endpoint_error(self):
+        service = "test-service"
+        query = "test query"
+        bad_query_response = '{"errs":["Lots of errors"],"msg":""}'
+        timeout = 0.03
+        with mock.patch('socket.socket') as mock_sock:
+            mock_sock.return_value.recv.return_value = bad_query_response
+            with self.assertRaises(EnvironmentError):
+                self.api.query(service=service, query=query, timeout=timeout)
+
+    def test_errs_field_error(self):
+        service = "test-service"
+        query = "test query"
+        bad_query_response = '{"bad_field_name":[], "msg":""}'
+        timeout = 0.03
+        with mock.patch('socket.socket') as mock_sock:
+            mock_sock.return_value.recv.return_value = bad_query_response
+            with self.assertRaises(KeyError):
+                self.api.query(service=service, query=query, timeout=timeout)
+
+    def test_msg_field_error(self):
+        service = "test-service"
+        query = "test query"
+        bad_query_response = '{"bad_field_name":[], "errs":""}'
+        timeout = 0.03
+        with mock.patch('socket.socket') as mock_sock:
+            mock_sock.return_value.recv.return_value = bad_query_response
+            with self.assertRaises(KeyError):
+                self.api.query(service=service, query=query, timeout=timeout)
 
 
 if __name__ == '__main__':
