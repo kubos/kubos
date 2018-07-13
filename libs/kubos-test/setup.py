2ad818afa96513d00a0b2c3c125d51a272316285
jacoffey3 <jacoffey3@gmail.com>
2018-07-13 18:03:07 -0400

    changes per review

--- libs/kubos-test/setup.py
+++ libs/kubos-test/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-A setuptools based setup module.
+A setuptools based setup module for the kubos test package.
 See:
 https://github.com/pypa/sampleproject
 """
@@ -10,5 +10,6 @@ from setuptools import setup
 setup(name='kubos_test',
       version='0.1.0',
       description='Manual integration testing library for KubOS Services',
-      py_modules=["kubos_test"]
+      py_modules=["kubos_test"],
+      install_requires=["app_api"]
       )
