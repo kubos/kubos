d3479cc8b4a4231ffee2da6ce49158627be4e747
jacoffey3 <jacoffey3@gmail.com>
2018-08-02 09:54:12 -0500

    cleaned out the example app from testing I was doing

--- examples/python-mission-application/mission-app.py
+++ examples/python-mission-application/mission-app.py
@@ -21,62 +21,79 @@ import toml
 import time
 import app_api
 
+
 MANIFEST = toml.load('manifest.toml')
 SERVICES = app_api.Services()
+LOGFILE = '/home/kubos/mission_app_log'
 
 
 def on_boot():
 
-    print "OnBoot logic\n"
+    with open(LOGFILE, 'a+') as log:
+        log.write("OnBoot logic\n")
     query = '{ apps { active, app { uuid, name, version, author } } }'
     while True:
         try:
-            print("##################################################")
-            print("I'm performing recurring system housekeeping logic")
-            print("Querying the application service for active applications!")
-            print("Query: {}".format(query))
-            apps = SERVICES.query(service="app-service", query=query)
-            print("Active applications are: {}".format(apps))
+            with open(LOGFILE, 'a+') as log:
+                log.write("###############################################\n")
+                log.write("I'm performing recurring system housekeeping\n")
+                log.write("Querying for active applications!\n")
+                log.write("Query: {}\n".format(query))
+                apps = SERVICES.query(service="app-service", query=query)
+                log.write("Active applications are: {}\n".format(apps))
         except Exception as e:
-            print("Housekeeping caused an error: {},{},{}".format(
-                type(e), e.args, e))
+            with open(LOGFILE, 'a+') as log:
+                log.write("Housekeeping caused an error: {},{},{}\n".format(
+                    type(e), e.args, e))
 
-        print("\n\n")
+        with open(LOGFILE, 'a+') as log:
+            log.write("\n\n")
         time.sleep(10)
 
 
 def on_command(cmd_args):
 
-    print "OnCommand logic"
+    with open(LOGFILE, 'a+') as log:
+        log.write("OnCommand logic\n")
 
     if cmd_args.cmd_string == 'safemode':
         if cmd_args.cmd_int > 0:
-            print(
-                "I'm been commanded to go into safemode for {} minutes".format(
-                    cmd_args.cmd_int))
-            print("send commands to hardware to go into safemode")
-            time.sleep(cmd_args.cmd_int * 60)
-            print("send commands to hardware to resume normal operation")
+            with open(LOGFILE, 'a+') as log:
+                log.write(
+                    "Going into safemode for {} seconds\n".format(
+                        cmd_args.cmd_int))
+                log.write("Sending commands to hardware to go into safemode\n")
+                time.sleep(cmd_args.cmd_int)
+                log.write("Sending commands to hardware to normal operation\n")
         else:
-            raise ValueError("Command Integer must be positive and non-zero")
-    elif cmd_args.cmd_string == 'manifest':
-        print "My manifest information: {}".format(MANIFEST)
+            raise ValueError("Command Integer must be positive and non-zero\n")
     else:
-        raise ValueError(
-            'Command "{}" not supported!'.format(cmd_args.cmd_string))
+        # If no command args are given, just print manifest info
+        with open(LOGFILE, 'a+') as log:
+            log.write("My manifest information: {}\n".format(MANIFEST))
 
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--run', '-r', nargs=1, default='OnBoot')
     parser.add_argument(
-        '--cmd_string',
+        '-r',
+        '--run',
+        nargs=1,
+        default='OnBoot',
+        help='Determines run behavior. Either "OnBoot" or "OnCommand"',
+        required=True)
+    parser.add_argument(
         '-s',
-        help='Command Argument String passed into OnCommand behavior')
+        '--cmd_string',
+        help='Command Argument String passed into OnCommand behavior',
+        required=False)
     parser.add_argument(
-        '--cmd_int', '-i', type=int,
-        help='Command Argument Integer passed into OnCommand behavior')
+        '-i',
+        '--cmd_int',
+        type=int,
+        help='Command Argument Integer passed into OnCommand behavior',
+        required=False)
 
     args = parser.parse_args()
 
