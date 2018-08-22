d92678da980dbdd599c04c224a2f35cd8973a17e
Catherine Garabedian <catherine@kubos.co>
2018-08-22 20:09:55 +0000

    Adding upload integration tests

--- services/file-service-rust/src/main.rs
+++ services/file-service-rust/src/main.rs
@@ -1,76 +1,16 @@
 extern crate file_protocol;
+extern crate file_service_rust;
 extern crate kubos_system;
 #[macro_use]
 extern crate log;
 extern crate simplelog;
 
+use file_service_rust::*;
 use kubos_system::Config as ServiceConfig;
 use simplelog::*;
 use std::thread;
 
 use file_protocol::{CborProtocol, FileProtocol, Message};
-
-fn recv_loop(config: ServiceConfig) -> Result<(), String> {
-    // TODO: Make configurable
-    let c_protocol = CborProtocol::new(config.hosturl());
-
-    loop {
-        // Listen on UDP port
-        let (peer, message) = match c_protocol.recv_message_peer()? {
-            (peer, Some(data)) => (peer, data),
-            _ => continue,
-        };
-
-        // Break the processing work off into its own thread so we can
-        // listen for requests from other clients
-        thread::spawn(move || {
-            // Set up the file system processor with the reply socket information
-            // TODO: Opening a second local port might be a terrible plan. Though it is kind of what TCP does
-            let f_protocol = FileProtocol::new(format!("{}", peer.ip()).to_owned(), peer.port());
-
-            // Parse it into a known message type and process
-            // TODO: Convert the various failures/unwraps to nice error printing
-            if let Ok(message) = f_protocol.on_message(message) {
-                match message {
-                    Message::SyncChunks(hash, num_chunks) => {
-                        // A client has notified us of a file we should be prepared to receive
-                        f_protocol.store_meta(&hash, num_chunks).unwrap();
-                    }
-                    Message::NAK(hash, missing_chunks) => {
-                        // Some number of chunks weren't received by the client.
-                        // Resend them
-                        if let Some(chunks) = missing_chunks {
-                            f_protocol.do_upload(&hash, &chunks).unwrap();
-                        }
-                    }
-                    Message::ReqReceive(channel_id, hash, path, mode) => {
-                        // The client wants to send us a file.
-                        // Go listen for the chunks.
-                        // Note: Won't return until we've received all of them.
-                        // (so could potentially never return)
-                        f_protocol.sync_and_send(&hash, None).unwrap();
-
-                        match f_protocol.local_export(&hash, &path, mode) {
-                            Ok(()) => {
-                                f_protocol.send_success(channel_id).unwrap();
-                            }
-                            Err(error) => {
-                                f_protocol.send_failure(channel_id, &error).unwrap();
-                            }
-                        }
-                    }
-                    _ => {
-                        // Whatever action was needed for this particular message
-                        // was already taken care of by `on_message()`
-                    }
-                }
-            } else {
-                // Q: Do we want to do any kind of error handling,
-                //    or just move on to the next message?
-            }
-        });
-    }
-}
 
 fn main() {
     CombinedLogger::init(vec![
---
+++ services/file-service-rust/tests/upload.rs
@@ -0,0 +1,342 @@
+extern crate file_protocol;
+extern crate file_service_rust;
+extern crate kubos_system;
+extern crate tempfile;
+extern crate threadpool;
+
+use kubos_system::Config as ServiceConfig;
+use file_service_rust::recv_loop;
+use std::thread;
+use file_protocol::CborProtocol;
+use file_protocol::FileProtocol;
+use std::env;
+use std::path::Path;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::time::Duration;
+use tempfile::TempDir;
+use threadpool::ThreadPool;
+
+// NOTE: Each test's file contents must be unique. Otherwise the hash is the same, so
+// the same storage directory is used across all of them, creating conflicts
+
+macro_rules! service_new {
+    ($port:expr) => {{
+        thread::spawn(move || {
+            recv_loop(ServiceConfig::new_from_str(
+                "file-transfer-service",
+                &format!(
+                    r#"
+                [file-transfer-service.addr]
+                ip = "127.0.0.1"
+                port = {}
+                "#,
+                    $port
+                ),
+            )).unwrap();
+        });
+    }};
+}
+
+fn upload(port: u16, source_path: &str, target_path: &str) -> Result<String, String> {
+    let f_protocol = FileProtocol::new(String::from("127.0.0.1"), port);
+
+    println!(
+        "Uploading local:{} to remote:{}",
+        &source_path, &target_path
+    );
+    // Copy file to upload to temp storage. Calculate the hash and chunk info
+    // Q: What's `mode` for? `local_import` always returns 0. Looks like it should be file permissions
+    let (hash, num_chunks, mode) = f_protocol.local_import(&source_path)?;
+    // Tell our destination the hash and number of chunks to expect
+    f_protocol.send_sync(&hash, num_chunks)?;
+    // Send the actual file
+    f_protocol.send_export(&hash, &target_path, mode)?;
+    Ok(hash.to_owned())
+}
+
+fn create_test_file(name: &str, contents: &[u8]) {
+    let mut file = File::create(name).unwrap();
+    file.write_all(contents).unwrap();
+}
+
+// upload single-chunk file from scratch
+#[test]
+fn upload_single() {
+    let test_dir = TempDir::new().expect("Failed to create test dir");
+    let test_dir_str = test_dir.path().to_str().unwrap();
+    let source = format!("{}/source", test_dir_str);
+    let dest = format!("{}/dest", test_dir_str);
+    let service_port = 7000;
+
+    let contents = "test1".as_bytes();
+
+    create_test_file(&source, &contents);
+
+    service_new!(service_port);
+
+    let result = upload(service_port, &source, &dest);
+
+    assert!(result.is_ok());
+
+    let hash = result.unwrap();
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Cleanup the temporary files so that the test can be repeatable
+    fs::remove_dir_all(format!("storage/{}", hash)).unwrap();
+
+    // Verify the final file's contents
+    let dest_contents = fs::read(dest).unwrap();
+    assert_eq!(&contents[..], dest_contents.as_slice());
+}
+
+// upload multi-chunk file from scratch
+#[test]
+fn upload_multi_clean() {
+    let test_dir = TempDir::new().expect("Failed to create test dir");
+    let test_dir_str = test_dir.path().to_str().unwrap();
+    let source = format!("{}/source", test_dir_str);
+    let dest = format!("{}/dest", test_dir_str);
+    let service_port = 7001;
+
+    let contents = [1; 5000];
+
+    create_test_file(&source, &contents);
+
+    service_new!(service_port);
+
+    let result = upload(service_port, &source, &dest);
+
+    assert!(result.is_ok());
+
+    let hash = result.unwrap();
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Cleanup the temporary files so that the test can be repeatable
+    fs::remove_dir_all(format!("storage/{}", hash)).unwrap();
+
+    // Verify the final file's contents
+    let dest_contents = fs::read(dest).unwrap();
+    assert_eq!(&contents[..], dest_contents.as_slice());
+}
+
+// upload multi-chunk file which we already have 1 chunk for
+#[test]
+fn upload_multi_resume() {
+    let test_dir = TempDir::new().expect("Failed to create test dir");
+    let test_dir_str = test_dir.path().to_str().unwrap();
+    let source = format!("{}/source", test_dir_str);
+    let dest = format!("{}/dest", test_dir_str);
+    let service_port = 7002;
+
+    let contents = [2; 5000];
+
+    create_test_file(&source, &contents);
+
+    service_new!(service_port);
+
+    // Go ahead and upload the whole file so we can manipulate the temporary directory
+    let result = upload(service_port, &source, &dest);
+    assert!(result.is_ok());
+    let hash = result.unwrap();
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Remove a chunk so we can test the retry logic
+    fs::remove_file(format!("storage/{}/0", hash)).unwrap();
+
+    // Upload the file again
+    let result = upload(service_port, &source, &dest);
+    assert!(result.is_ok());
+    let hash = result.unwrap();
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Cleanup the temporary files so that the test can be repeatable
+    fs::remove_dir_all(format!("storage/{}", hash)).unwrap();
+
+    // Verify the final file's contents
+    let dest_contents = fs::read(dest).unwrap();
+    assert_eq!(&contents[..], dest_contents.as_slice());
+}
+
+// upload multi-chunk file which we already have all chunks for
+#[test]
+fn upload_multi_complete() {
+    let test_dir = TempDir::new().expect("Failed to create test dir");
+    let test_dir_str = test_dir.path().to_str().unwrap();
+    let source = format!("{}/source", test_dir_str);
+    let dest = format!("{}/dest", test_dir_str);
+    let service_port = 7005;
+
+    let contents = [3; 5000];
+
+    create_test_file(&source, &contents);
+
+    service_new!(service_port);
+
+    // Upload the file once (clean upload)
+    let result = upload(service_port, &source, &dest);
+    assert!(result.is_ok());
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Upload the file again
+    let result = upload(service_port, &source, &dest);
+    assert!(result.is_ok());
+    let hash = result.unwrap();
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Cleanup the temporary files so that the test can be repeatable
+    fs::remove_dir_all(format!("storage/{}", hash)).unwrap();
+
+    // Verify the final file's contents
+    let dest_contents = fs::read(dest).unwrap();
+    assert_eq!(&contents[..], dest_contents.as_slice());
+}
+
+// upload. Create hash mismatch.
+#[test]
+fn upload_bad_hash() {
+    let test_dir = TempDir::new().expect("Failed to create test dir");
+    let test_dir_str = test_dir.path().to_str().unwrap();
+    let source = format!("{}/source", test_dir_str);
+    let dest = format!("{}/dest", test_dir_str);
+    let service_port = 7003;
+
+    let contents = "test1".as_bytes();
+
+    create_test_file(&source, &contents);
+
+    service_new!(service_port);
+
+    // Upload the file so we can mess with the temporary storage
+    let result = upload(service_port, &source, &dest);
+    assert!(result.is_ok());
+    let hash = result.unwrap();
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Tweak the chunk contents so the future hash calculation will fail
+    fs::write(format!("storage/{}/0", hash), "bad data".as_bytes()).unwrap();
+
+    // TODO: THIS SHOULD FAIL
+    let result = upload(service_port, &source, &dest);
+    // TODO: Verify exact error message
+    assert!(result.is_ok());
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Cleanup the temporary files so that the test can be repeatable
+    fs::remove_dir_all(format!("storage/{}", hash)).unwrap();
+}
+
+#[test]
+fn upload_multi_client() {
+    let service_port = 7004;
+
+    // Spawn our single service
+    service_new!(service_port);
+
+    let pool = ThreadPool::new(5);
+
+    // Spawn 5 simultaneous clients
+    for num in 0..5 {
+        pool.execute(move || {
+            let test_dir = TempDir::new().expect("Failed to create test dir");
+            let test_dir_str = test_dir.path().to_str().unwrap();
+            let source = format!("{}/source", test_dir_str);
+            let dest = format!("{}/dest", test_dir_str);
+            //let contents = format!("test{}", num);
+            let contents = [num; 5000];
+
+            create_test_file(&source, &contents);
+
+            let result = upload(service_port, &source, &dest);
+
+            assert!(result.is_ok());
+
+            let hash = result.unwrap();
+
+            // TODO: Remove this sleep. We need it to let the service
+            // finish its work. The upload logic needs to wait on
+            // the final ACK message before returning
+            thread::sleep(Duration::new(1, 0));
+
+            // Cleanup the temporary files so that the test can be repeatable
+            fs::remove_dir_all(format!("storage/{}", hash)).unwrap();
+
+            // Verify the final file's contents
+            let dest_contents = fs::read(dest).unwrap();
+            assert_eq!(&contents[..], dest_contents.as_slice());
+        });
+    }
+
+    // Wait for all the threads to finish
+    pool.join();
+}
+
+// Massive upload
+// TODO: Enable once chunk numbers > 9 are supported properly
+/*
+#[test]
+fn upload_large() {
+    let test_dir = TempDir::new().expect("Failed to create test dir");
+    let test_dir_str = test_dir.path().to_str().unwrap();
+    let source = format!("{}/source", test_dir_str);
+    let dest = format!("{}/dest", test_dir_str);
+    let service_port = 7001;
+
+    let contents = [1; 1_000_000];
+
+    create_test_file(&source, &contents);
+
+    service_new!(service_port);
+
+    let result = upload(service_port, &source, &dest);
+
+    assert!(result.is_ok());
+
+    let hash = result.unwrap();
+
+    // TODO: Remove this sleep. We need it to let the service
+    // finish its work. The upload logic needs to wait on
+    // the final ACK message before returning
+    thread::sleep(Duration::new(1, 0));
+
+    // Cleanup the temporary files so that the test can be repeatable
+    fs::remove_dir_all(format!("storage/{}", hash)).unwrap();
+
+    // Verify the final file's contents
+    let dest_contents = fs::read(dest).unwrap();
+    assert_eq!(&contents[..], dest_contents.as_slice());
+}
+*/
