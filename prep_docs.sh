014acbbbb705ca50b09aadd49cd6ae3dad686b92
Catherine Garabedian <catherine@kubos.co>
2019-01-21 19:30:07 +0000

    Adding python app api to path for doc generation

--- prep_docs.sh
+++ prep_docs.sh
@@ -12,6 +12,9 @@ echo "Tag for docs: $latest_tag"
 # Place current docs tag into docs conf
 sed -i "s/0.0.0/$latest_tag/g" docs/conf.py
 
+# Add the app API to the system path so that its docs can be auto-generated
+pip install ./apis/app-api/python
+
 echo "Generating new docs"
 ./tools/gendocs.py --version $latest_tag
 
