2259253673bda5fbd2ab57d0bc5b5d263037cb85
Ryan Plauche <ryan@kubos.co>
2019-01-31 16:02:53 -0600

    Removing more references to yotta-based tools

--- prep_docs.sh
+++ prep_docs.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Link in yotta modules for those rust
-# modules that need them
-# Needed to run cargo kubos -c doc
-./tools/kubos_link.py
-
 latest_tag=`git tag --sort=-creatordate --sort=-v:refname| head -n 1`
 
 echo "Tag for docs: $latest_tag"
