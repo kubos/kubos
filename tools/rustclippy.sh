a06cb055bf60d9cd31114a507c1e4691cf217d56
Ryan Plauche <ryan@kubos.co>
2019-02-04 16:11:12 -0600

    Removing a few more references to yotta based tools.

--- tools/rustclippy.sh
+++ tools/rustclippy.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-# Link in yotta modules for those rust
-# modules that need them
-./tools/kubos_link.py
-
 # Run clippy linter
-cargo kubos -c clippy -- -- -A clippy::implicit_hasher -A clippy::type_complexity -A clippy::cyclomatic_complexity
+cargo clippy -- -A clippy::implicit_hasher -A clippy::type_complexity -A clippy::cyclomatic_complexity
