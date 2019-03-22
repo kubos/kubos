eaeefe42fa686f45609b0fb5694b32d18ee65a87
Catherine Garabedian <catherine@kubos.co>
2019-03-22 21:04:27 +0000

    Adding unit tests

--- apis/nsl-simplex-s3/src/lib.rs
+++ apis/nsl-simplex-s3/src/lib.rs
@@ -14,69 +14,74 @@
 // limitations under the License.
 //
 
-use failure::Error;
+#[cfg(test)]
+mod tests;
 
-const ACK: [u8; 3] = [0xAA, 0x05, 0x00];
-const NAK: [u8; 3] = [0xAA, 0x05, 0xFF];
-const RESP_LEN: u8 = 3;
+use failure::{bail, Error};
+use log::*;
+use rust_uart::Connection;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
+const ACK: &[u8] = &[0xAA, 0x05, 0x00];
+const NAK: &[u8] = &[0xAA, 0x05, 0xFF];
+const RESP_LEN: usize = 3;
+
+#[derive(Clone)]
 pub struct SimplexS3 {
     /// Device connection structure
     pub conn: Arc<Mutex<Connection>>,
 }
 
 impl SimplexS3 {
-    pub fn new(
-        bus: &str,
-        baud_rate: serial::BaudRate,
-        log_recv: Receiver<(Header, Vec<u8>)>,
-        response_recv: Receiver<(Header, Vec<u8>)>,
-    ) -> OEMResult<OEM6> {
+    pub fn new(bus: &str) -> Result<SimplexS3, Error> {
         let settings = serial::PortSettings {
-            baud_rate,
-            char_size: CHAR_SIZE,
-            parity: PARITY,
-            stop_bits: STOP_BITS,
-            flow_control: FLOW_CONTROL,
+            baud_rate: serial::Baud38400,
+            char_size: serial::Bits8,
+            parity: serial::ParityNone,
+            stop_bits: serial::Stop1,
+            flow_control: serial::FlowNone,
         };
 
-        let conn = Arc::new(Mutex::new(Connection::from_path(bus, settings, TIMEOUT)?));
+        let conn = Arc::new(Mutex::new(Connection::from_path(
+            bus,
+            settings,
+            Duration::from_millis(60),
+        )?));
 
-        Ok(SimplexS3 {
-            conn
-        })
+        Ok(SimplexS3 { conn })
     }
-    
+
     pub fn send_beacon(&self, msg: &[u8]) -> Result<(), Error> {
-        
         if msg.len() > 35 {
             bail!("Packet too large");
         }
-        
-        let mut packet = vec![ 0x50, 0x50, 0x50 ];
-        packet.push(msg);
-        
+
+        let mut packet = vec![0x50, 0x50, 0x50];
+        packet.extend_from_slice(msg);
+
         // TODO: Check for busy signal
-        
-        let conn = self.conn.lock()?;
-        
+
+        let conn = self
+            .conn
+            .lock()
+            .or_else(|err| bail!("Mutex is poisoned: {:?}", err))?;
+
         for _ in 0..5 {
-        
-            conn.write(packet)?;
-            
+            conn.write(&packet)?;
+
             let response = conn.read(RESP_LEN, Duration::from_millis(10))?;
-            
-            match response {
+
+            match response.as_slice() {
                 ACK => {
                     info!("Sent beacon");
                     return Ok(());
                 }
                 NAK => continue,
-                other => warn!("Unknown resp from simplex: {:?}", other)
+                other => warn!("Unknown resp from simplex: {:?}", other),
             }
         }
-        
+
         bail!("Failed to send message");
     }
 }
-
