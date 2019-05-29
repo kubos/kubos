d7a8d5f3b5a39fb4472d3716d5db6fd959d8e71f
Seabass247 <sebjfk@gmail.com>
2019-05-29 13:12:30 -0400

    copywright blocks & contributions

--- apis/nosengine-rust/src/ffi/mod.rs
+++ apis/nosengine-rust/src/ffi/mod.rs
@@ -1,3 +1,21 @@
+//
+// Copyright (C) 2018 Kubos Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Contributed by: Timothy Scott (tmscott@mix.wvu.edu)
+//
+
 //! This module wraps the C interface of NOSEngine. It provides the basic functionality
 //! for base buses and communication. All functions that are prefixed by `NE_` are the unsafe
 //! C functions. All others are safe wrappers around the C functions.
