41e08b85382821fd58a469c3f516db3b239ffe76
Seabass247 <sebjfk@gmail.com>
2019-06-10 11:41:05 -0400

    cargo fmt novatel api

--- apis/novatel-oem6-api/examples/gps-test.rs
+++ apis/novatel-oem6-api/examples/gps-test.rs
@@ -133,7 +133,13 @@ fn main() -> OEMResult<()> {
     let (response_send, response_recv) = sync_channel(5);
     let (response_abbrv_send, response_abbrv_recv) = sync_channel(5);
 
-    let oem = OEM6::new(bus, BaudRate::Baud9600, log_recv, response_recv, response_abbrv_recv)?;
+    let oem = OEM6::new(
+        bus,
+        BaudRate::Baud9600,
+        log_recv,
+        response_recv,
+        response_abbrv_recv,
+    )?;
 
     let rx_conn = oem.conn.clone();
 
