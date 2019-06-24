9a6e45cdab002dc51331bfc31c02bbafcfb2419d
Ryan Plauche <ryan@kubos.co>
2019-06-24 16:38:49 -0500

    Local development setup doc

---
+++ tools/kubos_verify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script is intended to verify an environment`s ability
+# to compile, run and test the core KubOS services and libraries.
+
+cargo test --package kubos-app-service
+cargo test --package telemetry-service
+cargo test --package monitor-service
+cargo test --package shell-service
+cargo test --package file-service
+
+python3 hal/python-hal/i2c/test_i2c.py
+cd apis/app-api/python; python3 test_app_api.py; cd ../../..
