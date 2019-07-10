1fa8e386fff0a3ff4246f82884d28a2e6290d81e
Ryan Plauche <ryan@kubos.co>
2019-07-10 11:16:13 -0500

    Updating Python shebang interpreters

--- apis/app-api/python/app_api.py
+++ apis/app-api/python/app_api.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2018 Kubos Corporation
 # Licensed under the Apache License, Version 2.0
 # See LICENSE file for details.
--- examples/python-service/service/models.py
+++ examples/python-service/service/models.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright 2017 Kubos Corporation
 # Licensed under the Apache License, Version 2.0
