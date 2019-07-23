905d362a888fb9dfbacf9781833700b5702ec8a1
Seabass247 <sebjfk@gmail.com>
2019-07-23 11:40:17 -0400

    fix tests

--- apis/novatel-oem6-api/src/tests/mod.rs
+++ apis/novatel-oem6-api/src/tests/mod.rs
@@ -21,23 +21,26 @@ use std::sync::mpsc::sync_channel;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
+/// A macro for mocking a new OEM6 device instance.
 #[macro_export]
 macro_rules! mock_new {
     ($mock:ident) => {{
         let (log_send, log_recv) = sync_channel(5);
         let (response_send, response_recv) = sync_channel(5);
+        let (response_abbrv_send, response_abbrv_recv) = sync_channel(5);
 
         let conn = Arc::new(Mutex::new(Connection {
             stream: Box::new($mock),
         }));
         let rx_conn = conn.clone();
 
-        thread::spawn(move || read_thread(&rx_conn, &log_send, &response_send));
+        thread::spawn(move || read_thread(&rx_conn, &log_send, &response_send, &response_abbrv_send));
 
         OEM6 {
             conn,
             log_recv: Arc::new(Mutex::new(log_recv)),
             response_recv: Arc::new(Mutex::new(response_recv)),
+            response_abbrv_recv: Arc::new(Mutex::new(response_abbrv_recv)),
         }
     }};
 }
