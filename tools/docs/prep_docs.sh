399afdf3d8be8f637b313ba0b0f6a7bc58479e71
Ryan Plauche <ryan@kubos.co>
2019-07-26 16:49:18 -0500

    Moved docs gen to python3 and organized scripts. Removed some old scripts. Did a bit of cleanup on other scripts. Moved remaining python2 stuff to python3 in docker

--- tools/docs/prep_docs.sh
+++ tools/docs/prep_docs.sh
@@ -9,10 +9,7 @@ echo "Tag for docs: $latest_tag"
 # Place current docs tag into docs conf
 sed -i "s/0.0.0/$latest_tag/g" docs/conf.py
 
-# Add the app API to the system path so that its docs can be auto-generated
-pip install ./apis/app-api/python
-
 echo "Generating new docs"
-./tools/gendocs.py --version $latest_tag
+./tools/docs/gendocs.py --version $latest_tag
 
 chmod a+rw -R html/
