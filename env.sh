26bc4471967e0818945f4b60e8af4507b88df677
Ryan Plauche <ryan@kubos.co>
2020-02-06 08:55:32 -0600

    Moved release opt-level to 1 to fix process spawning issues?? Pinned nix for shell-protocol. Moved back to bootlin toolchain

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CC=/home/vagrant/arm-2010q1/bin/arm-uclinuxeabi-gcc
-export CXX=/home/vagrant/arm-2010q1/bin/arm-uclinuxeabi-g++
-#export CC=/home/vagrant/armv7m-m3-uclibc-2018/bin/arm-buildroot-uclinux-uclibcgnueabi-gcc
-#export CXX=/home/vagrant/armv7m-m3-uclibc-2018/bin/arm-buildroot-uclinux-uclibcgnueabi-g++
+#export CC=/home/vagrant/arm-2010q1/bin/arm-uclinuxeabi-gcc
+#export CXX=/home/vagrant/arm-2010q1/bin/arm-uclinuxeabi-g++
+export CC=/home/vagrant/armv7m-m3-uclibc-2018/bin/arm-buildroot-uclinux-uclibcgnueabi-gcc
+export CXX=/home/vagrant/armv7m-m3-uclibc-2018/bin/arm-buildroot-uclinux-uclibcgnueabi-g++
 export XARGO_RUST_SRC=/vagrant/repos/rust/src
