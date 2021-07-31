8d3a4c69ec1d0cf073765a9f6d678a539e3280ea
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-07-30 21:52:31 -0700

    update deploy script to commit to GH pages

--- tools/docs/deploy_docs.sh
+++ tools/docs/deploy_docs.sh
@@ -3,5 +3,20 @@
 latest_tag=`git tag --sort=-creatordate --sort=-v:refname| head -n 1`
 
 echo "Uploading new docs.."
-git clone https://github.com/kubos/pm-tools
-./pm-tools/docs/upload.py $latest_tag ./html/
+# git clone https://github.com/kubos/pm-tools
+# ./pm-tools/docs/upload.py $latest_tag ./html/
+
+[ ! -z "$GH_NAME" ] && git config user.name "$GH_NAME"
+[ ! -z "$GH_EMAIL" ] && git config user.email "$GH_EMAIL"
+
+MAIN_BRANCH=$(git symbolic-ref --short HEAD)
+
+git stash
+git branch --delete --force gh-pages
+git checkout --orphan gh-pages
+git add -f ./html/
+git commit -m "Deploy docs (commit '$latest_tag' )to GitHub pages [ci skip]"
+# git filter-branch -f --prune-empty --subdirectory-filter ./html/ &&
+git push -f origin gh-pages
+git checkout $MAIN_BRANCH
+git stash apply || :
